752a7839c97310b944c25b3825cd9ae2cd45d4a9
micheal-w-wells <micheal.w.wells@gmail.com>
2019-05-09 09:53:59 -0700

    get rid of angular stuff, going to use react

--- AngularApp/src/app/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app-constants';
-export * from './app-routes.enum';
-export * from './rest.api.const';
-export * from './app.enum';
--- services/app_api/main/db/create.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE db_prod;
-CREATE DATABASE db_dev;
-CREATE DATABASE db_test;
