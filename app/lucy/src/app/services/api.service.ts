49924906f50c015933bb7129d52b90e57cc8f28d
Amir Shayegh <shayegh@me.com>
2019-06-13 10:28:51 -0700

    commented API Service

--- app/lucy/src/app/services/api.service.ts
+++ app/lucy/src/app/services/api.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SsoService } from './sso.service';
 
+/*------------------------------------------------------------------------*/
 export enum APIRequestMethod {
   PUT,
   POST,
   GET,
+}
+
+export interface APIRequestResult {
+  success: boolean
+  response: any
 }
 
 export enum APIErrorDescision {
@@ -20,14 +26,12 @@ export interface APIError {
   error: any
   attempts: number
 }
+ /*------------------------------------------------------------------------*/
 
-export interface APIRequestResult {
-  success: boolean
-  response: any
-}
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
   private MAX_NUMBER_OF_API_RETRY: number = 3;
@@ -44,11 +48,13 @@ export class ApiService {
     });
   }
 
+  /*------------------------------------Requests------------------------------------*/
   /**
    * Make an API call and get the response.
    * @param method GET PUT POST
    * @param endpoint string
    * @param body any
+   * @returns Success | Fail & Response
    */
   public async request(method: APIRequestMethod, endpoint: string, body: any): Promise<APIRequestResult> {
     switch (method) {
@@ -68,6 +74,7 @@ export class ApiService {
    * @param method GET PUT POST
    * @param endpoint string
    * @param body any
+   * @returns Success | Fail & Response
    */
   private async requestRetry(method: APIRequestMethod, endpoint: string, body: any, attempts: number): Promise<APIRequestResult> {
     switch (method) {
@@ -86,6 +93,7 @@ export class ApiService {
    * If an Error occurs, try to handle it with handleError()
    * @param endpoint string
    * @param body JSON
+   * @returns Success | Fail & Response
    */
   private async postCall(endpoint: string, body: any, attempts: number): Promise<APIRequestResult> {
     console.log("POST: ")
@@ -119,6 +127,7 @@ export class ApiService {
   * If an Error occurs, try to handle it with handleError()
   * @param endpoint string
   * @param body JSON
+  * @returns Success | Fail & Response
   */
   private async putCall(endpoint: string, body: any, attempts: number): Promise<APIRequestResult> {
     console.log("PUT: ")
@@ -153,6 +162,7 @@ export class ApiService {
    * If an Error occurs, try to handle it with handleError()
    * @param endpoint 
    * @param body 
+   * @returns Success | Fail & Response
    */
   private async getCall(endpoint: string, attempts: number): Promise<APIRequestResult> {
     try {
@@ -177,18 +187,14 @@ export class ApiService {
     }
   }
 
-  private async hendleErrorDescision(error: APIError, descision: APIErrorDescision): Promise<APIRequestResult> {
-    switch (descision) {
-      case APIErrorDescision.Retry:
-        return await this.requestRetry(error.method, error.endpoint, error.body, error.attempts);
-      case APIErrorDescision.Stop:
-        return {
-          success: false,
-          response: null
-        }
-    }
-  }
+  /*------------------------------------END Of Requests------------------------------------*/
 
+  /*------------------------------------ERROR Handling------------------------------------*/
+  /**
+   * Handle Erros based on status codes
+   * @param error 
+   * @returns Result of hendleErrorDescision()
+   */
   private async handleError(error: APIError): Promise<APIRequestResult> {
     switch (error.error.status) {
       case 401:
@@ -203,6 +209,36 @@ export class ApiService {
     }
   }
 
+  /**
+   * Handle An API Error and descision: 
+   *  * if Retry, retry the request.
+   *  * if Stop, Send fail responce to api caller
+   * @param error 
+   * @param descision 
+   * @returns 
+   *  Based on descision:
+   *  * Retry: 
+   *    * result of requestRetry()
+   *  * Stop: 
+   *    * !Success & empty response
+   */
+  private async hendleErrorDescision(error: APIError, descision: APIErrorDescision): Promise<APIRequestResult> {
+    switch (descision) {
+      case APIErrorDescision.Retry:
+        return await this.requestRetry(error.method, error.endpoint, error.body, error.attempts);
+      case APIErrorDescision.Stop:
+        return {
+          success: false,
+          response: null
+        }
+    }
+  }
+
+  /**
+   * Decide how to handle a 401 error.
+   * @param error 
+   * @returns APIErrorDescision Retry | Stop
+   */
   private async decideOn401(error: APIError): Promise<APIErrorDescision> {
     if (error.attempts >= this.MAX_NUMBER_OF_API_RETRY) {
       return APIErrorDescision.Stop;
@@ -210,4 +246,6 @@ export class ApiService {
     const refreshed = await this.ssoService.refreshToken();
     return refreshed ? APIErrorDescision.Retry : APIErrorDescision.Stop;
   }
+
+  /*------------------------------------END OF ERROR Handling------------------------------------*/
 }
