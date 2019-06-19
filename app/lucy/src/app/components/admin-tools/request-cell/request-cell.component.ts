2b66875670bccee35eeb061b3f64d8d5dae352b4
Amir Shayegh <shayegh@me.com>
2019-06-19 09:59:22 -0700

    added access request endpoint

--- app/lucy/src/app/components/admin-tools/request-cell/request-cell.component.ts
+++ app/lucy/src/app/components/admin-tools/request-cell/request-cell.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AccessRequest } from 'src/app/models/accessRequest';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-request-cell',
+  selector: 'tr[app-request-cell]',
   templateUrl: './request-cell.component.html',
   styleUrls: ['./request-cell.component.css']
 })
 export class RequestCellComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+
+  responding: boolean = false
+
+  get userUserame(): string {
+    if (this.request === undefined) {
+      return "";
+    } else {
+      return this.request.requester.preferredUsername;
+    }
+  }
+
+  get userName(): string {
+    if (this.request === undefined) {
+      return "";
+    } else {
+      return (this.request.requester.firstName + 
+        " " + this.request.requester.lastName);
+    }
+  }
+
+  get userRole(): string {
+    if (this.request === undefined) {
+      return "";
+    } else {
+      return this.userService.getUserAccessCode(this.request.requester).role;
+    }
+  }
+
+  get requestNote(): string {
+    if (this.request === undefined) {
+      return "";
+    } else {
+      return this.request.requestNote;
+    }
+  }
+
+  get requestedAccessCode(): string {
+    if (this.request === undefined) {
+      return "";
+    } else {
+      return this.request.requestedAccessCode.role;
+    }
+  }
+
+  @Input() request: AccessRequest;
 
   ngOnInit() {
   }
-
 }
