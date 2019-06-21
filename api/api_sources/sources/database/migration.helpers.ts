39fff0c4d7ad71f348641f03dca15cee52bb7c09
fwpushan <pushan@freshworks.io>
2019-06-21 11:00:11 -0700

    Documenation - 3

--- api/api_sources/sources/database/migration.helpers.ts
+++ api/api_sources/sources/database/migration.helpers.ts
@@ -1,5 +1,23 @@
+//
+// Typeorm Database migration helper classes
+//
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Pushan Mitra on 2019-05-10.
 /**
- * Migration helper classes
+ * Imports
  */
 import { Connection, MigrationInterface } from 'typeorm';
 import { LoggerBase} from '../server/logger';
@@ -193,6 +211,5 @@ export class AppDatabaseMigrationManager extends LoggerBase {
         }
     }
 }
-
 // -----------------------------------------------------------------------------------------
 
