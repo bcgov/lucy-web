30a001fabf25b7b3c43eee4ce883d8745db02573
fwpushan <pushan@freshworks.io>
2019-06-21 11:52:02 -0700

    Documenation - 5

--- api/api_sources/sources/server/core/auth.middleware.ts
+++ api/api_sources/sources/server/core/auth.middleware.ts
@@ -1,4 +1,24 @@
-// Application Auth Middleware
+//
+// Auth Middleware
+//
+// Copyright © 2019 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Pushan Mitra on 2019-06-6.
+/**
+ * Imports
+ */
 import * as passport from 'passport';
 import * as express from 'express';
 import * as assert from 'assert';
@@ -9,6 +29,10 @@ import { LoggerBase } from '../logger';
 import { errorBody } from './common.error.handler';
 import { BCHelperLib } from '../../libs/utilities/bc.helpers';
 
+/**
+ * @description Model interface for Middleware result
+ * @export interface MiddlewareValidationResult
+ */
 export interface MiddlewareValidationResult {
     message?: string;
     code?: number;
@@ -215,5 +239,4 @@ export const roleAuthenticationMiddleware = (roles: RolesCodeValue[]) => {
 export const adminOnlyRoute = () => {
     return roleAuthenticationMiddleware([RolesCodeValue.admin]);
 };
-
-// -----------------
+// -----------------------------------------------------------------------------------------------------------
