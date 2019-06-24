a3bc776a8f8aed5031f9c106bb71f8898d6c1898
fwpushan <pushan@freshworks.io>
2019-06-24 15:42:57 -0700

    Add Support to run app in Openshif with config support

--- api/.pipeline/lib/config.js
+++ api/.pipeline/lib/config.js
@@ -6,7 +6,7 @@ const name = 'lucy-api'
 
 const phases = {
   build: {namespace:'8ecbmv-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'8ecbmv-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, host: `${name}-${changeId}-8ecbmv-dev.pathfinder.gov.bc.ca`},
+  dev: {namespace:'8ecbmv-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, host: `${name}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`},
   test: {namespace:'8ecbmv-test'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`, host: `${name}-8ecbmv-test.pathfinder.gov.bc.ca`},
   prod: {namespace:'8ecbmv-prod'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
