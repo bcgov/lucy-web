72f18b8dac163733ebefed579fc7112658da5026
fwpushan <pushan@freshworks.io>
2019-06-24 16:01:26 -0700

    Fix host name issue for app

--- app/.pipeline/lib/config.js
+++ app/.pipeline/lib/config.js
@@ -7,7 +7,7 @@ const apiName = 'lucy-api';
 
 const phases = {
   build: {namespace:'8ecbmv-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'8ecbmv-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, host: `${name}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`, apiHost:`${apiName}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`},
+  dev: {namespace:'8ecbmv-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, host: `${name}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`, apiHost:`${apiName}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}/`},
   test: {namespace:'8ecbmv-test'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`, host: `${name}-8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`},
   prod: {namespace:'8ecbmv-prod'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`, host: `${name}--8ecbmv-dev.pathfinder.gov.bc.ca/${changeId}`},
 };
