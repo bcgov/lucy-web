5d16b93a50eaf759ba85cccec0f82a5fa68afd20
fwpushan <pushan@freshworks.io>
2019-06-25 11:50:12 -0700

    Add app bootstrap service and config loading code

---
+++ app/lucy/src/app/services/bootstrap.service.ts
@@ -0,0 +1,29 @@
+import { Injectable} from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { ApiService, APIRequestMethod } from './api.service';
+import { AppConstants } from '../constants';
+
+@Injectable()
+export class AppBootService {
+    constructor(private api: HttpClient) {}
+    async loadConfig(): Promise<void> {
+        return new Promise(async res => {
+            try {
+                // Get config
+                const result: any = await this.api.get('/config').toPromise();
+                // Get API host
+                const host = result.apiHost || 'localhost:80';
+                // Create base url
+                const baseURL = `${host}/api/v1`;
+                // Setting base url to API const
+                AppConstants.API_baseURL = baseURL;
+                res();
+            } catch (excp) {
+                console.log(`Unable to load config due to excp ${excp}. Will use default config`);
+                res();
+            }
+        });
+    }
+}
+
+// -----------------------------------------------------------------------------------
