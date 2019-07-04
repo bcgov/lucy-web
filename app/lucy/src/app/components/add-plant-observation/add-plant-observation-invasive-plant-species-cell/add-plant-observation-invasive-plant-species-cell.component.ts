7c5fdc5b43da882946088fda904305b7dd4c2cdc
Amir Shayegh <shayegh@me.com>
2019-07-04 11:52:16 -0700

    implemeted dropdown usage in invasive plant species section

--- app/lucy/src/app/components/add-plant-observation/add-plant-observation-invasive-plant-species-cell/add-plant-observation-invasive-plant-species-cell.component.ts
+++ app/lucy/src/app/components/add-plant-observation/add-plant-observation-invasive-plant-species-cell/add-plant-observation-invasive-plant-species-cell.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPlantObservationInvasivePlantSpeciesCellComponent implements OnInit {
 
+  items: string[] = [`one`, `two`];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  dropdownSelectionChanged(value: string) {
+    console.log(value)
+  }
+
+  invasivePlantSpeciesChanged(value: string) {
+    console.log(value);
+  }
+
+  jurisdictionChanged(value: string) {
+    console.log(value);
+  }
+
+  densityChanged(value: string) {
+    console.log(value);
+  }
+
+  distributionChanged(value: string) {
+    console.log(value);
+  }
+
+  surveyModeChanged(value: string) {
+    console.log(value);
+  }
+
+  soilTextureCodeChanged(value: string) {
+    console.log(value);
+  }
+
+  specificUseCodeChanged(value: string) {
+    console.log(value);
   }
 
 }
