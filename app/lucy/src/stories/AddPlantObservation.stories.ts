a3d0f58edee860145a55d841744c6edd25f49202
micheal-w-wells <micheal.w.wells@gmail.com>
2019-07-16 15:32:36 -0700

    cleanup

--- app/lucy/src/stories/AddPlantObservation.stories.ts
+++ app/lucy/src/stories/AddPlantObservation.stories.ts
@@ -5,15 +5,14 @@ import { SideNavComponent } from 'src/app/components/add-plant-observation/side-
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AddPlantObservationBasicInformationComponent } from '../app/components/add-plant-observation/add-plant-observation-basic-information/add-plant-observation-basic-information.component';
 import { AddPlantObservationInvasivePlantSpeciesComponent } from '../app/components/add-plant-observation/add-plant-observation-invasive-plant-species/add-plant-observation-invasive-plant-species.component';
-//import {MatButtonModule, MatIconModule} from '@angular/material';
 import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
 import { withCssResources } from '@storybook/addon-cssresources';
 
-addDecorator(withCssResources)
+
 
 const stories = storiesOf('Storybook Knobs', module);
-
 stories.addDecorator(withKnobs);
+
 
 storiesOf('AddPlantObservationComponent', module)
  .addDecorator(
