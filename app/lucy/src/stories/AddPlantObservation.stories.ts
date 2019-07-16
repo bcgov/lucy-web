ccec9df5d1fca30212f7f49929739bcd1914a533
micheal-w-wells <micheal.w.wells@gmail.com>
2019-07-16 14:34:54 -0700

    fix css and install storybook add ons

--- app/lucy/src/stories/AddPlantObservation.stories.ts
+++ app/lucy/src/stories/AddPlantObservation.stories.ts
@@ -1,18 +1,24 @@
-import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { addDecorator, storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { AddPlantObservationComponent } from '../app/components/add-plant-observation/add-plant-observation.component';
 import { SideNavComponent } from 'src/app/components/add-plant-observation/side-nav/side-nav.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { AddPlantObservationBasicInformationComponent } from '../app/components/add-plant-observation/add-plant-observation-basic-information/add-plant-observation-basic-information.component';
+import { AddPlantObservationInvasivePlantSpeciesComponent } from '../app/components/add-plant-observation/add-plant-observation-invasive-plant-species/add-plant-observation-invasive-plant-species.component';
+//import {MatButtonModule, MatIconModule} from '@angular/material';
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
+import { withCssResources } from '@storybook/addon-cssresources';
 
+addDecorator(withCssResources)
 
+const stories = storiesOf('Storybook Knobs', module);
 
-
-
+stories.addDecorator(withKnobs);
 
 storiesOf('AddPlantObservationComponent', module)
  .addDecorator(
     moduleMetadata({
-      declarations: [],
+      declarations: [SideNavComponent,AddPlantObservationBasicInformationComponent,AddPlantObservationInvasivePlantSpeciesComponent],
       imports: [],
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
     }),
