d77be6de1fef7d9acbfbfb522d77d9b7d73771f3
micheal-w-wells <micheal.w.wells@gmail.com>
2019-07-16 11:21:00 -0700

    fix plant observation story

--- app/lucy/src/stories/AddPlantObservation.stories.ts
+++ app/lucy/src/stories/AddPlantObservation.stories.ts
@@ -2,6 +2,7 @@ import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { AddPlantObservationComponent } from '../app/components/add-plant-observation/add-plant-observation.component';
 import { SideNavComponent } from 'src/app/components/add-plant-observation/side-nav/side-nav.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 
 
