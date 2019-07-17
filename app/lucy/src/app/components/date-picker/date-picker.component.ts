6ac63121e267d2eae06fcc277871ae652560d34f
Amir Shayegh <shayegh@me.com>
2019-07-17 10:59:05 -0700

    created datepicker component

---
+++ app/lucy/src/app/components/date-picker/date-picker.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { FormMode } from 'src/app/models';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css']
+})
+export class DatePickerComponent implements OnInit {
+
+  get isViewMode(): boolean {
+    return this.mode === FormMode.View;
+  }
+
+  ///// Form Mode
+  private _mode: FormMode = FormMode.View;
+  // Get
+  get mode(): FormMode {
+    return this._mode;
+  }
+  // Set
+  @Input() set mode(mode: FormMode) {
+    this._mode = mode;
+  }
+  ////////////////////
+
+  ///// NG Bootstrap Date
+  private _ngDate: NgbDate;
+  // Get
+  get ngDate(): NgbDate {
+    return this._ngDate;
+  }
+  // Set
+  set ngDate(date: NgbDate) {
+    this._ngDate = date;
+  }
+  ////////////////////
+
+  ///// Date
+  private _date: Date;
+  // Get
+  get date(): Date {
+    return this._date;
+  }
+  // Set
+  @Input() set date(date: Date) {
+    if (!date) {
+      this._date = new Date();
+    } else {
+      this._date = date;
+    }
+    this.ngDate = new NgbDate(this._date.getFullYear(), this._date.getMonth(), this._date.getDay());
+  }
+  ////////////////////
+
+  @Output() selected = new EventEmitter<Date>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  dateChanged(value: NgbDate) {
+    console.dir(value);
+    this.date = new Date(`${value.year}-${value.month}-${value.day}`);
+    this.emitSelection();
+  }
+
+  emitSelection() {
+    this.selected.emit(this.date);
+  }
+
+}
