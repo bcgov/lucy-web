3968c42434dbb03a345036b41c2af47eb05a58d4
Amir Shayegh <shayegh@me.com>
2019-08-19 11:58:35 -0700

    created mechanical treatment service

---
+++ app/lucy/src/app/services/mechanical-treatment.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { ApiService, APIRequestMethod } from './api.service';
+import { MechanicalTreatment } from '../models/MechanicalTreatment';
+import { AppConstants } from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MechanicalTreatmentService {
+
+  constructor(private api: ApiService) { }
+
+  /**
+   * Creates json body for observation creation.
+   * @param observation object
+   */
+  private body(object: MechanicalTreatment): any {
+    /**
+     * Note: If your observation ends up having nested objects,
+     * this function will no longer work for compare() in this class.
+     * You'll need to create a similar function to "flatten" an observation.
+     */
+    const body = {
+      latitude: object.latitude,
+      longitude: object.longitude,
+      length: object.length,
+      width: object.width,
+      applicatorFirstName: object.applicatorFirstName,
+      applicatorLastName: object.applicatorLastName,
+      date: object.date,
+      paperFileReference: object.paperFileReference,
+      comment: object.comment,
+      observation: object.observation.observation_id,
+      species: object.species.species_id,
+      speciesAgency: object.speciesAgency.species_agency_code_id,
+      mechanicalMethod: 2,
+    };
+
+    return body;
+  }
+
+  public async submit(mechanicalTreatment: MechanicalTreatment): Promise<boolean> {
+    // You shouldn't use the object directly because api expects ids, not objects
+    const mechanicalTreatmentBody = this.body(mechanicalTreatment);
+    console.dir(mechanicalTreatment);
+
+    // Make the call
+    const response = await this.api.request(APIRequestMethod.POST, AppConstants.API_mechanicalTreatment, mechanicalTreatment);
+    if (response.success) {
+      console.dir(response.response);
+      // const _Id = response.response[`observation_id`];
+      // if (observation_Id) {
+      //   console.log(`Created successfully`);
+      //   return true;
+      // } else {
+      //   console.log(`Got a response, but something is off - id is missing`);
+      //   console.dir(response);
+      //   return false;
+      // }
+    } else {
+      console.log(`mechanical treatment creation failed`);
+      console.dir(response);
+      return false;
+    }
+  }
+}
