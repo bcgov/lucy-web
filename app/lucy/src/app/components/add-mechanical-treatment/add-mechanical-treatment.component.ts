69c32b9b3765e45ad460fbfb28bacd6d60c67d29
Amir Shayegh <shayegh@me.com>
2019-08-19 11:02:21 -0700

    handeling new routing system

---
+++ app/lucy/src/app/components/add-mechanical-treatment/add-mechanical-treatment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-mechanical-treatment',
+  templateUrl: './add-mechanical-treatment.component.html',
+  styleUrls: ['./add-mechanical-treatment.component.css']
+})
+export class AddMechanicalTreatmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
