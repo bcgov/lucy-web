d47613c7682be65d528ac26fbc2f7fd8a0f9d2f2
Amir Shayegh <shayegh@me.com>
2019-08-23 10:41:41 -0700

    added dif and edit functions to mechanical treatments service

--- app/lucy/src/app/services/mechanical-treatment.service.ts
+++ app/lucy/src/app/services/mechanical-treatment.service.ts
@@ -2,13 +2,22 @@ import { Injectable } from '@angular/core';
 import { ApiService, APIRequestMethod } from './api.service';
 import { MechanicalTreatment } from '../models/MechanicalTreatment';
 import { AppConstants } from '../constants';
+import { ObjectValidatorService } from './object-validator.service';
+
+export interface MechanicalTreatmentDiffResult {
+  changed: boolean;
+  newMechanicalTreatment: MechanicalTreatment;
+  originalMechanicalTreatment: MechanicalTreatment;
+  diffMessage: string;
+  changes: Object;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MechanicalTreatmentService {
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private objectValidator: ObjectValidatorService) { }
 
   /**
    * Creates json body for observation creation.
@@ -64,6 +73,84 @@ export class MechanicalTreatmentService {
     }
   }
 
+  /**
+   * Submit changes for a Mechanical Treatment.
+   * Send Full object
+   */
+  public async editMechanicalTreatment(mechanicalTreatment: MechanicalTreatment): Promise<boolean> {
+    // You shouldn't use the object directly because api expects ids, not objects
+    const body = this.body(mechanicalTreatment);
+
+    // Make the call
+    const response = await this.api.request(APIRequestMethod.PUT, AppConstants.API_observationWith(mechanicalTreatment.mechanical_treatment_id), body);
+    if (response.success) {
+      const _Id = response.response[`mechanical_treatment_id`];
+      if (_Id) {
+        console.log(`Edited successfully`);
+        return true;
+      } else {
+        console.log(`Got a response, but something is off - id is missing`);
+        console.dir(response);
+        return false;
+      }
+    } else {
+      console.log(`observation edit failed`);
+      console.dir(response);
+      return false;
+    }
+  }
+
+  /**
+   * Submit changes for a Mechanical Treatment.
+   * Send Only changes
+   */
+  public async editObservationChangeOnly(newMechanicalTreatment: MechanicalTreatment, oldMechanicalTreatment: MechanicalTreatment): Promise<boolean> {
+    const changes = await this.compare(newMechanicalTreatment, oldMechanicalTreatment);
+    console.dir(changes);
+    // Make the call
+    const response = await this.api.request(APIRequestMethod.PUT, AppConstants.API_observationWith(newMechanicalTreatment.mechanical_treatment_id), changes);
+    if (response.success) {
+      const observation_Id = response.response[`mechanical_treatment_id`];
+      if (observation_Id) {
+        console.log(`Edited successfully`);
+        return true;
+      } else {
+        console.log(`Got a response, but something is off - id is missing`);
+        console.dir(response);
+        return false;
+      }
+    } else {
+      console.log(`observation edit failed`);
+      console.dir(response);
+      return false;
+    }
+  }
+
+  /**
+   * Fetch and returns all MechanicalTreatment objects
+   */
+  public async getAll(): Promise<MechanicalTreatment[]> {
+    const response = await this.api.request(APIRequestMethod.GET, AppConstants.API_mechanicalTreatment, null);
+    if (response.success && Array.isArray(response.response) && this.objectValidator.isMechanicalTreatmentObject(response.response[0])) {
+      return response.response;
+    } else {
+      return[];
+    }
+  }
+
+  /**
+   * Fetch and return a specific Mechanical Treatment
+   * @param id Observation Id
+   */
+  public async getWithId(id: number): Promise<MechanicalTreatment | undefined> {
+    const response = await this.api.request(APIRequestMethod.GET, AppConstants.API_mechanicalTreatmentWith(id), null);
+    if (response.success && this.objectValidator.isMechanicalTreatmentObject(response.response)) {
+      return response.response;
+    } else {
+      return undefined;
+    }
+  }
+
   public getEmptyObject(): MechanicalTreatment {
     const object: MechanicalTreatment = {
       mechanical_treatment_id: -1,
@@ -83,4 +170,109 @@ export class MechanicalTreatmentService {
     };
     return object;
   }
+
+  ////////////////////////////// Diff //////////////////////////////
+
+  /**
+   * Compare specified MechanicalTreatment object with its original version.
+   * if undefinied is returned, there was a failure
+   * @param mechanicalTreatment MechanicalTreatment
+   * @return MechanicalTreatmentDiffResult | undefined
+   */
+  public async diffMechanicalTreatment(mechanicalTreatment: MechanicalTreatment): Promise<MechanicalTreatmentDiffResult | undefined> {
+    // 1) Fetch the latest original
+    const original = await this.getWithId(mechanicalTreatment.mechanical_treatment_id);
+    // if couldnt fetch the original, return undefind
+    if (!original) {
+      return undefined;
+    }
+    // 2) Compare
+    const changes = this.compare(mechanicalTreatment, original);
+    // if comparison fails, return undefinied.
+    if (!changes) {
+      return undefined;
+    }
+
+    // 3) Successfully diffed, generate response
+
+    // Convert keys from camel case:
+    const keys =  Object.keys(changes).map(x => {
+      const fromCamel = x.replace( /([A-Z])/g, ` $1` );
+      return fromCamel.charAt(0).toUpperCase() + fromCamel.slice(1);
+    });
+    const changedKeys = keys.join(`, `);
+    const changed = changedKeys.length > 1;
+    return {
+      changed: changed,
+      newMechanicalTreatment: mechanicalTreatment,
+      originalMechanicalTreatment: original,
+      diffMessage: changedKeys,
+      changes: changes
+    };
+  }
+
+  /**
+   * Compare a change Object to original MechanicalTreatment
+   * and return a string describing what changed.
+   * @param changes Object
+   * @param original MechanicalTreatment
+   */
+  private createDiffMessage(changes: Object, original: MechanicalTreatment): string {
+    // TODO: This function needs to be tweaked for usage: handle codes & date format
+    let msg = ``;
+    const originalObservation = JSON.parse(JSON.stringify(this.body(original)));
+    Object.keys(changes).forEach(function (key, index) {
+      const originaValue = originalObservation[key];
+      msg = `${msg}* ${key}: From: ${originaValue} -> To: ${changes[key]}\n`;
+    });
+    console.log(msg);
+    return msg;
+  }
+
+  /**
+   * Compare 2 MechanicalTreatment objects
+   * and return Object containing differences
+   * between the two.
+   * * Note: This function uses this.body()
+   * and doesnt use the objects that are passed in directly.
+   * @param mechanicalTreatment MechanicalTreatment
+   * @param original MechanicalTreatment
+   */
+  public compare(mechanicalTreatment: MechanicalTreatment, original: MechanicalTreatment): Promise<Object | undefined> {
+    const originalObservation = JSON.parse(JSON.stringify(this.body(original)));
+    const newObservation = JSON.parse(JSON.stringify(this.body(mechanicalTreatment)));
+    const r = this.diff(originalObservation, newObservation);
+    return r;
+  }
+
+  /**
+   * Compare 2 JSON object and
+   * return Object containing differences
+   * between the two.
+   * * Note: Keys should be in the same order
+   * @param obj1 JSON object
+   * @param obj2 JSON object
+   */
+  private diff(obj1: JSON, obj2: JSON): any {
+    const result = {};
+    if (Object.is(obj1, obj2)) {
+        return undefined;
+    }
+    if (!obj2 || typeof obj2 !== 'object') {
+        return obj2;
+    }
+    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+            result[key] = obj2[key];
+        }
+        if (typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+            const value = this.diff(obj1[key], obj2[key]);
+            if (value !== undefined) {
+                result[key] = value;
+            }
+        }
+    });
+    return result;
+  }
+  //////////////////////////////  Diff //////////////////////////////
 }
