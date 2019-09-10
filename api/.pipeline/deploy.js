66a1ecb80a5e2266f6c6c8c398e6c65bf97fd80b
fwpushan <pushan@freshworks.io>
2019-09-10 09:45:53 -0700

    Test Env Setup ----------------- 1. Create Pre-deployment stage 2. Merge Deploy APP and API static dev link stages --------------------------

--- api/.pipeline/deploy.js
+++ api/.pipeline/deploy.js
@@ -1,11 +1,7 @@
 'use strict';
 // Imports/require
 const settings = require('./lib/config.js');
-const taskDeployDB = require('./lib/deploy.db.js');
 const taskDeployAPI = require('./lib/deploy.js');
-
-// Deploying DB
-taskDeployDB(Object.assign(settings, { phase: settings.options.env}));
 
 // Deploying API app
 taskDeployAPI(Object.assign(settings, { phase: settings.options.env}));
