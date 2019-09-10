eb1847339a106592a78ed92c89d796fba0c5a57a
fwpushan <pushan@freshworks.io>
2019-09-10 14:55:41 -0700

    Remove schema spy build-1

--- api/.pipeline/buildtools.js
+++ api/.pipeline/buildtools.js
@@ -2,5 +2,5 @@
 const task = require('./lib/build.schemaspy.js')
 const settings = require('./lib/config.js')
 
-console.log('Not building schemaspy');
-// task(Object.assign(settings, { phase: 'build'}))
+
+task(Object.assign(settings, { phase: 'build'}))
