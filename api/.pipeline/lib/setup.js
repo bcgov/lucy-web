a625826aee7c53352ca38396794d0296dc09bdfb
fwpushan <pushan@freshworks.io>
2019-09-23 11:42:15 -0700

    Add wait-1

--- api/.pipeline/lib/setup.js
+++ api/.pipeline/lib/setup.js
@@ -28,10 +28,11 @@ module.exports = (settings) => {
     process.exit(0);
   }
   const imageStream = data[0];
+  const podName = `${phases[phase].name}${phases[phase].suffix}-setup`;
 
   objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/setup.pod.yaml`, {
     'param':{
-        'NAME': `${phases[phase].name}${phases[phase].suffix}-setup`,
+        'NAME': podName,
         'SUFFIX': phases[phase].suffix,
         'VERSION': phases[phase].tag,
         'CHANGE_ID': phases[phase].changeId,
@@ -43,5 +44,5 @@ module.exports = (settings) => {
   
   oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, instance)
   oc.applyAndDeploy(objects, phases[phase].instance)
-  
+  wait(`pod/${podName}`)
 }
