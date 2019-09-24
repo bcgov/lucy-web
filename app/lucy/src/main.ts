8cf0d3d8a2049f2f4bc882cae95ef171096bdf6b
Andrea Williams <andrea.williams@gov.bc.ca>
2019-09-24 10:19:12 -0700

    Remove logging & test buttons for prod env in front-end

--- app/lucy/src/main.ts
+++ app/lucy/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  window.console.log = function() {};
   enableProdMode();
 }
 
