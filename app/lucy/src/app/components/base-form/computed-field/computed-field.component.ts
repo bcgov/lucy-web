d09e4f151e3a1cdbd78371925cf984dcb2b2b0de
Amir Shayegh <shayegh@me.com>
2019-09-27 13:29:51 -0700

    added computer field component

---
+++ app/lucy/src/app/components/base-form/computed-field/computed-field.component.ts
@@ -0,0 +1,71 @@
+import { Component, Input, OnInit, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+
+export enum ComputationMethod {
+  calculateArea,
+}
+
+@Component({
+  selector: 'app-computed-field',
+  templateUrl: './computed-field.component.html',
+  styleUrls: ['./computed-field.component.css']
+})
+export class ComputedFieldComponent implements OnChanges, OnInit {
+  // Field header
+  @Input() header = '';
+  // Rules
+  @Input() computationRules: any;
+
+  private _formBody: any = {};
+  get formBody(): any {
+    return this._formBody;
+  }
+  @Input() set formBody(object: any) {
+    this._formBody = object;
+  }
+
+  private _value = `xx`;
+  get value(): string {
+    return this._value;
+  }
+  @Input() set value(value: string) {
+    this._value = value;
+  }
+
+  get fieldId(): string {
+    return this.header;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.dir(changes.formBody.currentValue);
+    // this.compute();
+  }
+
+  compute() {
+    if (!this.formBody || !this.computationRules || !this.computationRules[0])  {
+      return;
+    }
+    console.log('should compute');
+    console.log(this.computationRules);
+    console.log(`can compute: ${this.requiredFieldsForComputationExist()}`);
+
+  }
+
+  requiredFieldsForComputationExist(): boolean {
+    if (!this.formBody || !this.computationRules || !this.computationRules[0]) {
+      return false;
+    }
+
+    for (const key of this.computationRules[0].keys) {
+      if (this.formBody[key] === undefined) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
