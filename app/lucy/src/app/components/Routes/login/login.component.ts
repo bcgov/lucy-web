9fdeca444a14e0219ae5ad5c99edf0234b3ec528
Amir Shayegh <shayegh@me.com>
2019-10-08 12:11:17 -0700

    login navigates to the route before redirect to sso

--- app/lucy/src/app/components/Routes/login/login.component.ts
+++ app/lucy/src/app/components/Routes/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SsoService, SSOLoginProvider } from '../../../services/sso.service';
 import { LoadingService } from 'src/app/services/loading.service';
 import { StringConstants } from 'src/app/constants/string-constants';
+import { RouterService } from 'src/app/services/router.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   public appTitle = ``;
 
-  constructor(private loadingService: LoadingService, private ssoService?: SsoService) { }
+  constructor(private loadingService: LoadingService, private ssoService: SsoService, private router: RouterService, private cookieService: CookieService) { }
 
   ngOnInit() {
     this.setAppTitle();
@@ -30,6 +32,10 @@ export class LoginComponent implements OnInit {
   }
 
   loginWithIDIR() {
+    // If there is a route, store it in sessions to return to it after login
+    if (this.router.current.length > 0) {
+      localStorage.setItem('lastRoute', this.router.current);
+    }
     if (this.ssoService) {
       this.loadingService.add();
       this.ssoService.login(SSOLoginProvider.idir);
