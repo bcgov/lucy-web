241020ee8d1c70be7f2ac5cf8de7beb2f9b98f65
Amir Shayegh <shayegh@me.com>
2019-10-10 16:29:18 -0700

    dropdowns use displayValue in object as displayed value

--- app/lucy/src/app/app-routing.module.ts
+++ app/lucy/src/app/app-routing.module.ts
@@ -40,8 +40,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, ProfileComponent, UserInformationComponent]
+export const routingComponents = [LoginComponent, ProfileComponent, UserInformationComponent];
