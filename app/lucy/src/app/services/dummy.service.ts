40f2954637e41acf745eaf792be506b9ff4660fb
Amir Shayegh <shayegh@>
2019-10-11 11:45:36 -0700

    fixed missing generate generateTest() function in dummyService

--- app/lucy/src/app/services/dummy.service.ts
+++ app/lucy/src/app/services/dummy.service.ts
@@ -17,6 +17,7 @@ import * as moment from 'moment';
 import { MechanicalTreatment, MechanicalTreatmentMethodsCodes, MechanicalDisposalMethodsCodes, MechanicalSoilDisturbanceCodes, MechanicalRootRemovalCodes, MechanicalIssueCodes, MechanicalTreatmentProviders } from '../models/MechanicalTreatment';
 import { ObservationService } from './observation.service';
 import { MapMarker } from '../components/Utilities/map-preview/map-preview.component';
+import { UIConfigObject } from './form/form.service';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +25,203 @@ import { MapMarker } from '../components/Utilities/map-preview/map-preview.compo
 export class DummyService {
 
   constructor(private codeTables: CodeTableService, private observationService: ObservationService) { }
+
+  /**
+   * Generate testr data for given config
+   * @param configuration 
+   * @returns config with values & json body
+   */
+  async generateTest(configuration: UIConfigObject): Promise<{config: UIConfigObject, json: JSON}> {
+    const responseBody = {};
+    const config = { ...configuration};
+    for (const section of config.sections) {
+      for (const subsection of section.subSections) {
+        for (let i = 0; i < subsection.fields.length; i++) {
+          const field = subsection.fields[i];
+          if (field.isComputedField) {
+            continue;
+          }
+          if (field.isLocationField) {
+            const fakeLat = await this.fakeFieldValue(field.latitude);
+            field.latitude.value = fakeLat.fieldValue;
+            responseBody[field.latitude.key] = fakeLat.bodyValue;
+            const fakeLong = await this.fakeFieldValue(field.longitude);
+            field.longitude.value = fakeLong.fieldValue;
+            responseBody[field.longitude.key] = fakeLong.bodyValue;
+          } else {
+            const fake = await this.fakeFieldValue(field);
+            field.value = fake.fieldValue;
+            responseBody[field.key] = fake.bodyValue;
+          }
+        }
+      }
+    }
+    // console.dir(config);
+    // console.dir(this.responseBody)
+    return {
+      config: config,
+      json: JSON.parse(JSON.stringify(responseBody)),
+    };
+  }
+
+  private async fakeFieldValue(field: any): Promise<{fieldValue: any, bodyValue: any}> {
+    if (field.isDateField) {
+      const date = faker.date.past();
+      const fake = moment(date).format('YYYY-MM-DD');
+      return {
+        fieldValue: fake,
+        bodyValue: fake,
+      }
+    }
+
+    if (field.isCheckbox) {
+      const fake = faker.random.boolean();
+      return {
+        fieldValue: fake,
+        bodyValue: fake,
+      }
+    }
+
+    if (field.isTextAreaField) {
+      const fake = faker.lorem.sentences();
+      return {
+        fieldValue: fake,
+        bodyValue: fake,
+      }
+    }
+
+    if (field.isInputField && field.type.toLowerCase() === 'string') {
+      const fake = faker.lorem.word();
+      return {
+        fieldValue: fake,
+        bodyValue: fake,
+      }
+    }
+
+    if (field.isInputField && field.type.toLowerCase() === 'number') {
+      const fake = this.randomNumber(4, 20)
+      return {
+        fieldValue: `${fake}`,
+        bodyValue: `${fake}`,
+      }
+    }
+    
+    // Dropdown
+    if (field.isDropdown) {
+      const randomIndex = this.randomNumber(0, field.dropdown.length - 1);
+      const value = field.dropdown[randomIndex];
+      const vieldValue = value;
+      let selectedID = 0;
+      for (const key in value) {
+        if (key.toLowerCase().indexOf(`id`) !== -1) {
+          selectedID = value[key];
+          break;
+        }
+      }
+      return {
+        fieldValue: vieldValue,
+        bodyValue: selectedID,
+      }
+    }
+
+    if (field.key.toLowerCase() === `lat` || field.key.toLowerCase() === `latitude`) {
+      const value = this.randomLat();
+      return {
+        fieldValue: value,
+        bodyValue: value,
+      }
+    }
+
+    if (field.key.toLowerCase() === `lon` || field.key.toLowerCase() === `long` ||field.key.toLowerCase() === `longitude` ) {
+      const value = this.randomLong();
+      return {
+        fieldValue: value,
+        bodyValue: value,
+      }
+    }
+
+    console.log(`Unknown field ${field}`);
+    console.dir(field);
+    return {
+      fieldValue: undefined,
+      bodyValue: undefined,
+    }
+    
+  }
+
+  /**
+  * Generate a random longitude within bc
+  */
+  public randomLong(): number {
+    const a = this.randomNumber(0, 7);
+    const b = this.randomNumber(845602, 977180);
+    const z = `-12${a}.${b}`;
+    return +z;
+  }
+
+  /**
+   * Generate a random latitude within bc
+   */
+  public randomLat() {
+    const a = this.randomNumber(0, 8);
+    const b = this.randomNumber(713134, 202679);
+    const z = `5${a}.${b}`;
+    return +z;
+  }
+
+  private randomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+   /**
+   * Return a random date in string format
+   */
+  public randomDateString() {
+    const date = faker.date.past();
+    return moment(date).format('YYYY-MM-DD');
+  }
+
+  /**
+   * Generate a unique incremental id based on given observations
+   * @param observations objects
+   */
+  private getUniqueObservationId(observations: any[]): number {
+    if (observations.length < 1) {
+      return 0;
+    }
+    const usedIds: number[] = [];
+    for (const object of observations) {
+      usedIds.push(object.observation_id);
+    }
+
+    const sortedUsedIds = usedIds.sort((n1, n2) => n1 - n2);
+    return sortedUsedIds.pop() + 1;
+  }
+
+  /**
+   * Generate random coordinates in BC
+   * @param number of coordinates
+   */
+  public generateCoordinates(number: number): MapMarker[] {
+    const coordinates: MapMarker[] = [];
+    for (let _i = 0; _i < number; _i++) {
+      coordinates.push( {
+        latitude: this.randomLat(),
+        longitude: this.randomLong()
+      });
+    }
+    return coordinates;
+  }
+
+  public async createDummyObservations(number: number): Promise<any[]> {
+    return await this.createDummyObservationsOLD(number);
+  } 
+  
+   ////////////////// REMOVE EVEYRYTHING BELOW THIS LINE //////////////////
+   ////////////////// REMOVE EVEYRYTHING BELOW THIS LINE //////////////////
+   ////////////////// REMOVE EVEYRYTHING BELOW THIS LINE //////////////////
+   ////////////////// REMOVE EVEYRYTHING BELOW THIS LINE //////////////////
+   ////////////////// REMOVE EVEYRYTHING BELOW THIS LINE //////////////////
 
   /**
    * Return a random jurisdiction
@@ -250,7 +448,7 @@ export class DummyService {
    * Create and return a random array of observations
    * @param number of observations
    */
-  async createDummyObservations(number: number): Promise<Observation[]> {
+  async createDummyObservationsOLD(number: number): Promise<Observation[]> {
     const observations: Observation[] = [];
     for (let _i = 0; _i < number; _i++) {
       const observation = await this.createDummyObservation(observations);
@@ -338,31 +536,6 @@ export class DummyService {
     return observation;
   }
 
-  /**
-   * Generate a unique incremental id based on given observations
-   * @param observations objects
-   */
-  private getUniqueObservationId(observations: Observation[]): number {
-    if (observations.length < 1) {
-      return 0;
-    }
-    const usedIds: number[] = [];
-    for (const object of observations) {
-      usedIds.push(object.observation_id);
-    }
-
-    const sortedUsedIds = usedIds.sort((n1, n2) => n1 - n2);
-    return sortedUsedIds.pop() + 1;
-  }
-
-
-  /**
-   * Return a random date in string format
-   */
-  public randomDateString() {
-    const date = faker.date.past();
-    return moment(date).format('YYYY-MM-DD');
-  }
 
   /**
    * Generate a random number
@@ -371,41 +544,6 @@ export class DummyService {
    */
   public randomIntFromInterval(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-   /**
-   * Generate a random longitude within bc
-   */
-  public randomLong(): number {
-    const a = this.randomIntFromInterval(0, 7);
-    const b = this.randomIntFromInterval(845602, 977180);
-    const z = `-12${a}.${b}`;
-    return +z;
-  }
-
-  /**
-   * Generate a random latitude within bc
-   */
-  public randomLat() {
-    const a = this.randomIntFromInterval(0, 8);
-    const b = this.randomIntFromInterval(713134, 202679);
-    const z = `5${a}.${b}`;
-    return +z;
-  }
-
-  /**
-   * Generate random coordinates in BC
-   * @param number of coordinates
-   */
-  public generateCoordinates(number: number): MapMarker[] {
-    const coordinates: MapMarker[] = [];
-    for (let _i = 0; _i < number; _i++) {
-      coordinates.push( {
-        latitude: this.randomLat(),
-        longitude: this.randomLong()
-      });
-    }
-    return coordinates;
   }
 
 }
