6129c907c4c8a3a8f12cd81f505cb5cf41d03aec
fwpushan <pushan@freshworks.io>
2019-10-16 13:05:00 -0700

    Fix displayLabel issue for MT contractor, improve test

--- api/api_sources/sources/database/__tests__/observation.spec.ts
+++ api/api_sources/sources/database/__tests__/observation.spec.ts
@@ -22,7 +22,7 @@
  */
 
 import { expect, should } from 'chai';
-import { commonTestSetupAction, commonTestTearDownAction } from '../../test-helpers/testHelpers';
+import { commonTestSetupAction, commonTestTearDownAction, testModel } from '../../test-helpers/testHelpers';
 import {
     observationFactory,
     destroyObservation,
@@ -35,12 +35,26 @@ import {
     aspectCodeFactory,
     proposedActionCodeFactory
 } from '../factory';
-import { ObservationController
+import {
+    ObservationController
 } from '../models';
 import {
     speciesDensityCodeFactory,
     speciesDistributionCodeFactory
 } from '../factory';
+import {
+    ObservationSchema,
+    SpeciesDensityCodeSchema,
+    SpeciesDistributionCodeSchema,
+    SpeciesAgencyCodeSchema,
+    ObservationTypeCodeSchema,
+    SoilTextureCodeSchema,
+    ObservationGeometryCodeSchema,
+    SpecificUseCodeSchema,
+    SlopeCodeSchema,
+    AspectCodeSchema,
+    ProposedActionCodeSchema 
+} from '../database-schema';
 
 describe('Observation tests', () => {
     before(async () => {
@@ -55,21 +69,7 @@ describe('Observation tests', () => {
         const f = await observationFactory();
         should().exist(f);
         const obs = await ObservationController.shared.findById(f.observation_id);
-        should().exist(obs);
-        should().exist(obs.createdBy);
-        should().exist(obs.updatedBy);
-        should().exist(obs.jurisdiction);
-        should().exist(obs.species);
-        should().exist(obs.observationType);
-        should().exist(obs.speciesAgency);
-        should().exist(obs.density);
-        should().exist(obs.distribution);
-        should().exist(obs.soilTexture);
-        should().exist(obs.observationGeometry);
-        should().exist(obs.specificUseCode);
-        should().exist(obs.slopeCode);
-        should().exist(obs.aspectCode);
-        should().exist(obs.proposedAction);
+        testModel(obs, ObservationSchema.shared);
         expect(obs.createdBy.user_id).to.equal(f.createdBy.user_id);
         expect(obs.updatedBy.user_id).to.equal(f.updatedBy.user_id);
         expect(obs.observation_id).to.equal(f.observation_id);
@@ -79,61 +79,61 @@ describe('Observation tests', () => {
 
     it('should fetch species density code', async () => {
         const code = await speciesDensityCodeFactory(2);
-        should().exist(code);
+        testModel(code, SpeciesDensityCodeSchema.shared);
         expect(code.species_density_code_id).to.be.equals(2);
     });
 
     it('should fetch species density code', async () => {
         const code = await speciesDistributionCodeFactory(2);
-        should().exist(code);
+        testModel(code, SpeciesDistributionCodeSchema.shared);
         expect(code.species_distribution_code_id).to.be.equals(2);
     });
 
     it('should fetch species agency code', async () => {
         const code = await speciesAgencyCodeFactory(2);
-        should().exist(code);
+        testModel(code, SpeciesAgencyCodeSchema.shared);
         expect(code.species_agency_code_id).to.be.equals(2);
     });
 
     it('should fetch species observation type code', async () => {
         const code = await observationTypeCodeFactory(2);
-        should().exist(code);
+        testModel(code, ObservationTypeCodeSchema.shared);
         expect(code.observation_type_code_id).to.be.equals(2);
     });
 
     it('should fetch soil texture code', async () => {
         const code = await soilTextureCodeFactory(2);
-        should().exist(code);
+        testModel(code, SoilTextureCodeSchema.shared);
         expect(code.soil_texture_code_id).to.be.equals(2);
     });
 
     it('should fetch survey geometry code', async () => {
         const code = await observerGeometryCodeFactory(2);
-        should().exist(code);
+        testModel(code, ObservationGeometryCodeSchema.shared);
         expect(code.observation_geometry_code_id).to.be.equals(2);
     });
 
     it('should fetch specific use code', async () => {
         const code = await specificUseCodeFactory(2);
-        should().exist(code);
+        testModel(code, SpecificUseCodeSchema.shared);
         expect(code.specific_use_code_id).to.be.equals(2);
     });
 
     it('should fetch slope code', async () => {
         const code = await slopeCodeFactory(3);
-        should().exist(code);
+        testModel(code, SlopeCodeSchema.shared);
         expect(code.observation_slope_code_id).to.be.equal(3);
     });
 
     it('should fetch aspect code', async () => {
         const code = await aspectCodeFactory(3);
-        should().exist(code);
+        testModel(code, AspectCodeSchema.shared);
         expect(code.observation_aspect_code_id).to.be.equal(3);
     });
 
     it('should fetch proposed action code', async () => {
         const code = await proposedActionCodeFactory(2);
-        should().exist(code);
+        testModel(code, ProposedActionCodeSchema.shared);
         expect(code.observation_proposed_action_code_id).to.be.equal(2);
     });
 });
