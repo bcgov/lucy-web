766551d97cf6bc478062f8b25e2421dc8512d2ad
Andrea Williams <andrea.williams@gov.bc.ca>
2019-10-24 16:14:23 -0700

    Updated entries for Mechanical Method code

--- api/api_sources/schema-migration-sql/MechanicalMethodCodeData.sql
+++ api/api_sources/schema-migration-sql/MechanicalMethodCodeData.sql
@@ -2,7 +2,7 @@
 -- ## Inserting Item: 0  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('DIG','Digging');
+('NA','None');
 -- ## End of item: 0 ## --
 -- ## Inserting Item: 1  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
@@ -12,52 +12,52 @@ VALUES
 -- ## Inserting Item: 2  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('BUR','Controlled Burning');
+('DIG','Digging');
 -- ## End of item: 2 ## --
 -- ## Inserting Item: 3  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('CUL','Cultivation or till');
+('BUR','Controlled Burning');
 -- ## End of item: 3 ## --
 -- ## Inserting Item: 4  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('DED','Dead-heading');
+('CUL','Cultivation or till');
 -- ## End of item: 4 ## --
 -- ## Inserting Item: 5  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('FLA','Flaming / Tiger Torch burn');
+('DED','Dead-heading');
 -- ## End of item: 5 ## --
 -- ## Inserting Item: 6  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('HAN','Hand pulling');
+('FLA','Flaming / Tiger Torch burn');
 -- ## End of item: 6 ## --
 -- ## Inserting Item: 7  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('HWS','Hot water / Steam');
+('HAN','Hand pulling');
 -- ## End of item: 7 ## --
 -- ## Inserting Item: 8  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('MOW','Mowing');
+('HWS','Hot water / Steam');
 -- ## End of item: 8 ## --
 -- ## Inserting Item: 9  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('MUL','Mulching');
+('MOW','Mowing');
 -- ## End of item: 9 ## --
 -- ## Inserting Item: 10  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('SD','Suction dredging');
+('MUL','Mulching');
 -- ## End of item: 10 ## --
 -- ## Inserting Item: 11  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('SHM','Sheet Mulching');
+('PLT','Planting');
 -- ## End of item: 11 ## --
 -- ## Inserting Item: 12  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
@@ -67,25 +67,30 @@ VALUES
 -- ## Inserting Item: 13  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('TG','Targeted grazing');
+('SED','Seeding');
 -- ## End of item: 13 ## --
 -- ## Inserting Item: 14  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('TR','Tarping');
+('SHM','Sheet Mulching');
 -- ## End of item: 14 ## --
 -- ## Inserting Item: 15  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('SED','Seeding');
+('SD','Suction dredging');
 -- ## End of item: 15 ## --
 -- ## Inserting Item: 16  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('PLT','Planting');
+('TG','Targeted grazing');
 -- ## End of item: 16 ## --
 -- ## Inserting Item: 17  ## --
 INSERT INTO mechanical_method_code(mechanical_method_code, description)
 VALUES
-('CNV','Mechanical Method not recorded');
+('TR','Tarping');
 -- ## End of item: 17 ## --
+-- ## Inserting Item: 18  ## --
+INSERT INTO mechanical_method_code(mechanical_method_code, description)
+VALUES
+('CNV','Legacy');
+-- ## End of item: 18 ## --
