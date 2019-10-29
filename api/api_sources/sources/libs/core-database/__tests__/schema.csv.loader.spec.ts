b71fe657d6eefa3afc40ce5703ccedfa79a319f0
fwpushan <pushan@freshworks.io>
2019-10-29 11:35:32 -0700

    Fix test issue

--- api/api_sources/sources/libs/core-database/__tests__/schema.csv.loader.spec.ts
+++ api/api_sources/sources/libs/core-database/__tests__/schema.csv.loader.spec.ts
@@ -10,7 +10,7 @@ import { getSQLFileData } from '../sql.loader';
 const schema = new Test2Schema();
 describe('Test Schema CSV Loader', () => {
     it('should create csv', async () => {
-        const dryRun = process.env.ENVIRONMENT === 'local' ? true : false;
+        const dryRun = process.env.ENVIRONMENT === 'local' ? false : true;
         const report = await SchemaCSVLoader.shared.createImportMigrations(schema, dryRun);
         should().exist(report);
         if (!dryRun) {
