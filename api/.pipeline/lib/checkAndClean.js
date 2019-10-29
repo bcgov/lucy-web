7d3ff075a5bf987b0d0bc47cf092d131acd59fea
fwpushan <pushan@freshworks.io>
2019-10-29 10:36:58 -0700

    Handling resource not found error

--- api/.pipeline/lib/checkAndClean.js
+++ api/.pipeline/lib/checkAndClean.js
@@ -24,12 +24,16 @@
  * @description Check and delete existing resource
  */
 module.exports = (resourceName, oc) => {
-    const list = oc.get(resourceName) || [];
-    if (list.length === 0) {
-        console.log(`checkAndClean: No resource available with resource name: ${resourceName}`);
-    } else {
-        console.log(`checkAndClean: Cleaning resource => ${resourceName}`);
-        oc.delete([resourceName], {'ignore-not-found':'true', 'wait':'true'})
-    }
+   try {
+        const list = oc.get(resourceName) || [];
+        if (list.length === 0) {
+            console.log(`checkAndClean: No resource available with resource name: ${resourceName}`);
+        } else {
+            console.log(`checkAndClean: Cleaning resource => ${resourceName}`);
+            oc.delete([resourceName], {'ignore-not-found':'true', 'wait':'true'});
+        }
+   } catch(excp) {
+       console.log(`Resource ${resourceName} not available [${excp}]`);
+   }
 };
 // ---------------------------------------
