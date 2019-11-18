0e513adb24b5f6e28ca6d42a4dfb24659570cddd
Andrea Williams <andrea.williams@gov.bc.ca>
2019-11-18 12:01:28 -0800

    Add SpeciesTreatment schema & migration

---
+++ api/api_sources/sources/database/migrations/1574106157222-CreateSpeciesTreatment.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+import { AppDBMigrator } from '../applicationSchemaInterface';
+import { SpeciesTreatmentSchema } from '../database-schema';
+
+export class CreateSpeciesTreatment1574106157222 extends AppDBMigrator implements MigrationInterface {
+
+    speciesTreatmentSchema: SpeciesTreatmentSchema;
+
+    /**
+     * Setup
+     */
+    setup() {
+        // Adding Herbicide init schema to migrator
+        this.speciesTreatmentSchema = new SpeciesTreatmentSchema();
+        this.addSchemaInitVersion(this.speciesTreatmentSchema);
+        this.addDataImportMigration(this.speciesTreatmentSchema, 'init');
+    }
+
+    /**
+     * UP: Create DB method
+     */
+    public async up(queryRunner: QueryRunner): Promise<any> {
+         // Start Log
+         this.log('[START]', 'UP');
+         // Running all up migration files
+         await this.runQuerySqlFiles(this.upMigrations(), queryRunner);
+         await this.runSQLFileAsync(this.importMigrations(), queryRunner);
+         this.log('[END]', 'UP');
+    }
+
+    /**
+     * Down: Revert
+     */
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        this.log('[STAR]', 'DOWN');
+        await queryRunner.query(this.speciesTreatmentSchema.dropTable());
+        this.log('[END]', 'DOWN');
+    }
+
+}
