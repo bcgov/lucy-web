79019f6bff4a9de743dd637bb280c34c39e82d37
Andrea Williams <andrea.williams@gov.bc.ca>
2019-11-25 15:09:53 -0800

    Front end chemical treatment custom components in progress

---
+++ app/lucy/src/app/components/Routes/add-entry/chemical-treatment-custom/species-treated/species-treated.component.ts
@@ -0,0 +1,53 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CodeTableService } from 'src/app/services/code-table.service';
+import { InvasivePlantSpecies } from 'src/app/models/observation';
+import * as faker from 'faker';
+
+@Component({
+  selector: 'app-species-treated',
+  templateUrl: './species-treated.component.html',
+  styleUrls: ['./species-treated.component.css']
+})
+export class SpeciesTreatedComponent implements OnInit {
+
+  private _speciesBeingTreated: InvasivePlantSpecies[];
+  private _speciesNotBeingTreated: InvasivePlantSpecies[];
+    species: InvasivePlantSpecies[];
+
+
+  constructor(private codeTables: CodeTableService) { }
+
+  async ngOnInit() {
+    this.species = await this.codeTables.getInvasivePlantSpecies();
+    for (let i = 0; i < 2; i++) {
+        this._speciesBeingTreated.push(this.species[faker.random.number(this.species.length)]);
+    }
+    for (let i = 0; i < 2; i++) {
+        this._speciesNotBeingTreated.push(this.species[faker.random.number(this.species.length)]);
+    }
+
+  }
+
+  // TODO speciesBeingTreated is list of plant species taken from ???
+  set speciesBeingTreated(s: InvasivePlantSpecies[]) {
+    for (const elem of s) {
+        this._speciesBeingTreated.push(elem);
+    }
+  }
+
+  get speciesBeingTreated(): InvasivePlantSpecies[] {
+      return this._speciesBeingTreated;
+  }
+
+  // TODO speciesNotBeingTreated is list of plant species that have been observed within the treatment area but are not being addressed by the current treatment record
+  set speciesNotBeingTreated(s: InvasivePlantSpecies[]) {
+      for (const elem of s) {
+        this._speciesNotBeingTreated.push(elem);
+      }
+  }
+
+  get speciesNotBeingTreated(): InvasivePlantSpecies[] {
+      return this._speciesNotBeingTreated;
+  }
+}
