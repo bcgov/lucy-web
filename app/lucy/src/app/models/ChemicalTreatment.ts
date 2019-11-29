70141bb3c68f0f2b2be06396937be30eb75e8b18
Andrea Williams <andrea.williams@gov.bc.ca>
2019-11-29 11:30:50 -0800

    Front-end chemical treatment form in progress

---
+++ app/lucy/src/app/models/ChemicalTreatment.ts
@@ -0,0 +1,13 @@
+
+
+export interface HerbicideCodes {
+    herbicide_id: number;
+    herbicideCode: string;
+    compositeName: string;
+    activeIngredient: string;
+    tradeName: string;
+    pmraRegNum: number;
+    formulation: string;
+    applicationRate: number;
+    applicationUnits: string;
+}
