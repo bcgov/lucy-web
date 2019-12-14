099fbbf048684ca7126d43cc6e8dff29cf399165
fwpushan <pushan@freshworks.io>
2019-12-13 16:07:33 -0800

    BCGW Integration ----------------------- 1. Create geo utility 2. Create service class to connect BCGW/WFS service 3. Tests -----------------------------------

--- api/api_sources/sources/libs/utilities/geoMap.utility.ts
+++ api/api_sources/sources/libs/utilities/geoMap.utility.ts
@@ -46,7 +46,7 @@ export interface PointTuple {
 /**
  * @description Spatial Location tuple
  */
-export interface LocationTuple {
+export interface GeoLocation {
     latitude: number;
     longitude: number;
 }
@@ -55,6 +55,8 @@ export interface LocationTuple {
  * @description Utility to performing mapping function
  */
 export class GeoMapUtility {
+
+    static e = 0.081819218048345;
     /**
      * @description Convert Degree to radian
      * @param number deg
@@ -87,11 +89,95 @@ export class GeoMapUtility {
      * @param number longitude
      * @param number latitude
      */
-    static webMercatorToLongitudeLatitude(x: number, y: number): LocationTuple {
+    static webMercatorToLongitudeLatitude(x: number, y: number): GeoLocation {
         const longitude = GeoMapUtility.radianToDegree(x / a);
         const latitude = 90 - 2 * GeoMapUtility.radianToDegree(Math.atan(Math.exp(-y / a)));
         return {latitude, longitude};
     }
+
+    static distance(loc1: GeoLocation, loc2: GeoLocation, isInKM: boolean = false): number {
+        let lon1 = loc1.longitude;
+        let lat1 = loc1.latitude;
+        let lon2 = loc2.longitude;
+        let lat2 = loc2.latitude;
+        const deg2rad = 0.017453292519943295; // === Math.PI / 180
+        const cos = Math.cos;
+        lat1 *= deg2rad;
+        lon1 *= deg2rad;
+        lat2 *= deg2rad;
+        lon2 *= deg2rad;
+        const diam = 12742 * 1000; // Diameter of the earth in km (2 * 6371)
+        const dLat = lat2 - lat1;
+        const dLon = lon2 - lon1;
+        const x = (
+          (1 - cos(dLat)) +
+          (1 - cos(dLon)) * cos(lat1) * cos(lat2)
+        ) / 2;
+        const d = diam * Math.asin(Math.sqrt(x));
+        if (isInKM) {
+            return d / 1000;
+        }
+        return d;
+    }
+
+    static longitudeLatitudeCoordinateToAlbers(
+        latitude: number,
+        longitude: number
+      ): PointTuple {
+        const e2 = 2 * (1 / 298.257) - Math.pow(1 / 298.257, 2);
+        // const k = this.k0;
+        // const ep2 = e2 / (1 - e2);
+        const offsetX = 1000000;
+        const offsetY = 0;
+        const angle1 = this.degreeToRadian(50);
+        const angle2 = this.degreeToRadian(58.5);
+        const angle3 = this.degreeToRadian(45);
+        const angle4 = -126;
+        const angle1Squared = Math.pow(Math.sin(angle1), 2);
+        const angle2Squared = Math.pow(Math.sin(angle2), 2);
+        const latY = this.degreeToRadian(latitude);
+        const m1 = Math.cos(angle1) / Math.pow(1 - e2 * angle1Squared, 0.5);
+        const m2 = Math.cos(angle2) / Math.pow(1 - e2 * angle2Squared, 0.5);
+        const q1 =
+        (1 - e2) *
+        (Math.sin(angle1) / (1 - e2 * Math.pow(Math.sin(angle1), 2)) -
+            (1 / (2 * this.e)) *
+            Math.log(
+            (1 - this.e * Math.sin(angle1)) / (1 + this.e * Math.sin(angle1))
+            ));
+        const q2 =
+        (1 - e2) *
+        (Math.sin(angle2) / (1 - e2 * Math.pow(Math.sin(angle2), 2)) -
+            (1 / (2 * this.e)) *
+            Math.log(
+            (1 - this.e * Math.sin(angle2)) / (1 + this.e * Math.sin(angle2))
+            ));
+        const q0 =
+        (1 - e2) *
+        (Math.sin(angle3) / (1 - e2 * Math.pow(Math.sin(angle3), 2)) -
+            (1 / (2 * this.e)) *
+            Math.log(
+            (1 - this.e * Math.sin(angle3)) / (1 + this.e * Math.sin(angle3))
+            ));
+        const n = (Math.pow(m1, 2) - Math.pow(m2, 2)) / (q2 - q1);
+        const c = Math.pow(m1, 2) + n * q1;
+        const p0 = (a * Math.pow(c - n * q0, 0.5)) / n;
+        const q =
+        (1 - e2) *
+        (Math.sin(latY) / (1 - e2 * Math.pow(Math.sin(latY), 2)) -
+            (1 / (2 * this.e)) *
+            Math.log(
+            (1 - this.e * Math.sin(latY)) / (1 + this.e * Math.sin(latY))
+            ));
+        const p = (a * Math.pow(c - n * q, 0.5)) / n;
+        const theta = this.degreeToRadian(n * (longitude - angle4));
+        const albersX = p * Math.sin(theta) + offsetX;
+        const albersY = p0 - p * Math.cos(theta) + offsetY;
+        return {
+        x: albersX,
+        y: albersY
+        };
+      }
 }
 
 // ------------------------------
