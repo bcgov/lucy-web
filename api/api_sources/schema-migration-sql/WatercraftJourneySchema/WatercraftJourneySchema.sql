5d717d0b6b8f6e36a92d5b1d1ba28424f1d9e2ce
fwpushan <pushan@freshworks.io>
2020-01-21 11:54:50 -0800

    Add new data-model for watercraft jounrey-1

---
+++ api/api_sources/schema-migration-sql/WatercraftJourneySchema/WatercraftJourneySchema.sql
@@ -0,0 +1,39 @@
+-- ### Creating Table: watercraft_journey ### --
+
+        
+CREATE TABLE watercraft_journey ();
+ALTER TABLE watercraft_journey ADD COLUMN watercraft_journey_id SERIAL PRIMARY KEY;
+ALTER TABLE watercraft_journey ADD COLUMN journey_type INT NOT NULL DEFAULT 0;
+ALTER TABLE watercraft_journey ADD COLUMN watercraft_risk_assessment_id INT NOT NULL REFERENCES watercraft_risk_assessment(watercraft_risk_assessment_id) ON DELETE SET NULL;
+ALTER TABLE watercraft_journey ADD COLUMN water_body_id INT NOT NULL REFERENCES water_body(water_body_id) ON DELETE SET NULL;
+
+
+        
+-- ### Creating Comments on table ### --
+
+        
+COMMENT ON TABLE watercraft_journey IS 'Joining table for water-body details and a inspection';
+COMMENT ON COLUMN watercraft_journey.watercraft_journey_id IS 'Auto generated sequential primary key column.';
+COMMENT ON COLUMN watercraft_journey.journey_type IS 'Journey type of the associated regarding water body. i.e Previous (0) and next (1)';
+COMMENT ON COLUMN watercraft_journey.watercraft_risk_assessment_id IS 'Foreign key reference to Watercraft risk assessment table';
+COMMENT ON COLUMN watercraft_journey.water_body_id IS 'Foreign key reference to Water body detail table';
+
+
+        
+-- ### Creating Timestamp column ### --
+
+        
+ALTER TABLE watercraft_journey ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
+ALTER TABLE watercraft_journey ADD COLUMN updated_at TIMESTAMP DEFAULT NOW();
+COMMENT ON COLUMN watercraft_journey.created_at IS 'Timestamp column to check creation time of record';
+COMMENT ON COLUMN watercraft_journey.updated_at IS 'Timestamp column to check modify time of record';
+
+        
+-- ### Creating User Audit Columns ### --
+
+        
+ALTER TABLE watercraft_journey ADD COLUMN updated_by_user_id INT NULL DEFAULT NULL REFERENCES application_user(user_id) ON DELETE SET NULL;
+ALTER TABLE watercraft_journey ADD COLUMN created_by_user_id INT NULL DEFAULT NULL REFERENCES application_user(user_id) ON DELETE SET NULL;
+COMMENT ON COLUMN watercraft_journey.updated_by_user_id IS 'Audit column to track creator';
+COMMENT ON COLUMN watercraft_journey.created_by_user_id IS 'Audit column to track modifier';
+ -- ### End: watercraft_journey ### --
