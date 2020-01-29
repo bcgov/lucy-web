22421966be1e6f266a6469dc36399c5e8f3c687e
Amir Shayegh <shayegh@me.com>
2020-01-29 09:42:06 -0800

    code cleaning

--- app/lucy/src/app/services/export/export.service.ts
+++ app/lucy/src/app/services/export/export.service.ts
@@ -15,15 +15,15 @@ export class ExportService {
    * @param name 
    */
   public downloadCSV(objects: any, name: string | null) {
-    if (!objects) { return }
-    let fileName = name ? name : `export - ${Date().toString()}`
+    if (!objects) { return; }
+    const fileName = name ? name : `export - ${Date().toString()}`;
     const items = this.flattenJsonArray(objects);
-    const replacer = (key, value) => value === null ? '' : value // handle null values here
-    const header = Object.keys(items[0])
-    let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    csv.unshift(header.join(','))
-    csv = csv.join('\r\n')
-    var blob = new Blob([csv.toString()], { type: "text/json" });
+    const replacer = (key, value) => value === null ? '' : value; // handle null values here
+    const header = Object.keys(items[0]);
+    let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    csv = csv.join('\r\n');
+    const blob = new Blob([csv.toString()], { type: 'text/json' });
     saveAs(blob, `${fileName}.csv`);
   }
 
@@ -34,28 +34,28 @@ export class ExportService {
    * @param name file name
    */
   public downloadJSON(objects: any, name: string | null) {
-    if (!objects) { return }
-    let fileName = name ? name : `export - ${Date().toString()}`
-    let json = JSON.stringify(this.flattenJsonArray(objects));
-    var blob = new Blob([json.toString()], { type: "text/json" });
+    if (!objects) { return; }
+    const fileName = name ? name : `export - ${Date().toString()}`;
+    const json = JSON.stringify(this.flattenJsonArray(objects));
+    const blob = new Blob([json.toString()], { type: 'text/json' });
     saveAs(blob, `${fileName}.json`);
   }
 
-  /* 
+  /*
   Convert array of objects with nested objects into a flat structure
   */
   public flattenJsonArray(data: any[]): any {
-    var flatArray = [];
-    var flatObject = {};
+    const flatArray = [];
+    const flatObject = {};
 
-    for (var index = 0; index < data.length; index++) {
-      for (var prop in data[index]) {
+    for (let index = 0; index < data.length; index++) {
+      for (let prop in data[index]) {
 
-        var value = data[index][prop];
+        let value = data[index][prop];
 
-        if (Array.isArray(value)) {
-          for (var i = 0; i < value.length; i++) {
-            for (var inProp in value[i]) {
+        if (Array.isArray(data[index][prop])) {
+          for (let i = 0; i < value.length; i++) {
+            for (let inProp in value[i]) {
               flatObject[`${prop}-${inProp}`] = value[i][inProp]; // Key name for nested objext
             }
           }
