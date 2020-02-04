f8c09f0a91d61913fae9569afe988959ff7a4574
fwpushan <pushan@freshworks.io>
2020-02-04 14:26:22 -0800

    Remove commented code

--- api/api_sources/sources/server/modules/watercraftObservation/waterBody/waterBody.route.ts
+++ api/api_sources/sources/server/modules/watercraftObservation/waterBody/waterBody.route.ts
@@ -23,30 +23,4 @@ export class WaterBodyRouteController extends ResourceRouteController<WaterBodyC
     static get shared(): WaterBodyRouteController {
         return this.sharedInstance() as WaterBodyRouteController;
     }
-
-    /* async all(req: any, data?: any): Promise<[number, any]> {
-        // Get all entries
-        const d: [number, any] = await super.all(req, data);
-        if (d[0] === 200) {
-            const r: any = d[1];
-            let newR;
-            if (r instanceof Array) {
-                newR = r.map((v: WaterBody) => {
-                    return {
-                        name: v.name,
-                        water_body_id: v.water_body_id,
-                        latitude: v.latitude,
-                        longitude: v.longitude,
-                        abbrev: v.abbrev,
-                        closest: v.closest
-                    };
-                });
-            } else {
-                newR = r;
-            }
-            return [d[0], newR];
-        } else {
-            return d;
-        }
-    }*/
 }
