056c6f4e2c859d063ca0631b95d8f62f035ba1ba
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-06 14:25:30 -0800

    Set deleteCascade where needed for mechanical monitor; fix whitespace issue

--- app/lucy/src/app/components/Input/date-time-picker/date-time-picker.component.ts
+++ app/lucy/src/app/components/Input/date-time-picker/date-time-picker.component.ts
@@ -136,7 +136,7 @@ export class DateTimePickerComponent implements OnInit {
 
   camelize(str: string): string {
     return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-      return index == 0 ? word.toLowerCase() : word.toUpperCase();
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
     }).replace(/\s+/g, '');
   }
 
