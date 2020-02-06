ce052b79b3b37bbfb1398e537b50336417d93549
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-05 16:26:10 -0800

    Fix route unit tests

--- api/api_sources/sources/server/modules/monitor/mechanical/mechanical.monitor.route.spec.ts
+++ api/api_sources/sources/server/modules/monitor/mechanical/mechanical.monitor.route.spec.ts
@@ -47,21 +47,26 @@ import {
     destroyMechanicalMonitor,
     Destroyer,
     mechanicalMonitorUpdateSpecFactory,
-    destroyMechanicalTreatment
+    destroyMechanicalTreatment,
+    mechanicalTreatmentFactory
 } from '../../../../database/factory';
 import {
     MechanicalTreatmentController,
     MechanicalMonitorController,
     MechanicalMonitorSpec,
-    MechanicalMonitorUpdateSpec
+    MechanicalMonitorUpdateSpec,
+    MechanicalTreatment
 } from '../../../../database/models';
 import { viewerToken } from '../../../../test-helpers/token';
 import { MechanicalMonitorSchema } from '../../../../database/database-schema';
 
 describe('Test for mechanical monitoring', () => {
+    let mechTreatment: MechanicalTreatment;
+
     before(async () => {
         await commonTestSetupAction();
         await SharedExpressApp.initExpress();
+        mechTreatment = await mechanicalTreatmentFactory();
     });
     after(async () => {
         await commonTestTearDownAction();
@@ -101,6 +106,7 @@ describe('Test for mechanical monitoring', () => {
                 expect(data.mechanicalTreatmentID.mechanical_treatment_id).to.be.equal(create.mechanicalTreatmentID.mechanical_treatment_id);
 
                 await MechanicalMonitorController.shared.removeById(data.mechanical_monitor_id);
+                await MechanicalTreatmentController.shared.remove(mechTreatment);
             });
         });
     });
