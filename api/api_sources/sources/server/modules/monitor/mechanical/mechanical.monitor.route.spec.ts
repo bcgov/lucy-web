7379a17221dad4d09145b77d555500b58b06efd6
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-05 16:09:33 -0800

    Add mechanical monitor route unit tests that I forgot to do earlier...

--- api/api_sources/sources/server/modules/monitor/mechanical/mechanical.monitor.route.spec.ts
+++ api/api_sources/sources/server/modules/monitor/mechanical/mechanical.monitor.route.spec.ts
@@ -22,221 +22,204 @@
 //  */
 
 
-// /**
-//  * Imports
-//  */
-// import {
-//     should,
-//     expect
-// } from 'chai';
-// import * as request from 'supertest';
-// import { SharedExpressApp } from '../../../initializers';
-// import {
-//     verifySuccessBody,
-//     verifyErrorBody,
-//     commonTestSetupAction,
-//     commonTestTearDownAction,
-//     testRequest,
-//     AuthType,
-//     HttpMethodType
-// } from '../../../../test-helpers/testHelpers';
-// import {
-//     mechanicalMonitorCreateSpecFactory,
-//     RequestFactory,
-//     mechanicalMonitorFactory,
-//     destroyMechanicalMonitor,
-//     Destroyer
-// } from '../../../../database/factory';
-// import {
-//     MechanicalTreatmentController,
-//     MechanicalMonitorController,
-//     MechanicalMonitorSpec,
-//     MechanicalMonitorUpdateSpec
-// } from '../../../../database/models';
-// import { viewerToken } from '../../../../test-helpers/token';
-// import { MechanicalMonitorSchema } from '../../../../database/database-schema';
+/**
+ * Imports
+ */
+import {
+    should,
+    expect
+} from 'chai';
+import * as request from 'supertest';
+import { SharedExpressApp } from '../../../initializers';
+import {
+    verifySuccessBody,
+    verifyErrorBody,
+    commonTestSetupAction,
+    commonTestTearDownAction,
+    testRequest,
+    AuthType,
+    HttpMethodType
+} from '../../../../test-helpers/testHelpers';
+import {
+    mechanicalMonitorCreateSpecFactory,
+    RequestFactory,
+    mechanicalMonitorFactory,
+    destroyMechanicalMonitor,
+    Destroyer,
+    mechanicalMonitorUpdateSpecFactory,
+    destroyMechanicalTreatment
+} from '../../../../database/factory';
+import {
+    MechanicalTreatmentController,
+    MechanicalMonitorController,
+    MechanicalMonitorSpec,
+    MechanicalMonitorUpdateSpec
+} from '../../../../database/models';
+import { viewerToken } from '../../../../test-helpers/token';
+import { MechanicalMonitorSchema } from '../../../../database/database-schema';
 
-// describe('Test for mechanical monitoring', () => {
-//     before(async () => {
-//         await commonTestSetupAction();
-//         await SharedExpressApp.initExpress();
-//     });
-//     after(async () => {
-//         await commonTestTearDownAction();
-//     });
+describe('Test for mechanical monitoring', () => {
+    before(async () => {
+        await commonTestSetupAction();
+        await SharedExpressApp.initExpress();
+    });
+    after(async () => {
+        await commonTestTearDownAction();
+    });
 
-//     it('should create mechanical treatment for {admin}', async () => {
-//         const create = await mechanicalTreatmentCreateSpecFactory();
-//         const createReq = RequestFactory<MechanicalTreatmentSpec>(create, {
-//             schema: MechanicalTreatmentSchema.shared
-//         });
-//         await testRequest(SharedExpressApp.app , {
-//             url: '/api/treatment/mechanical/',
-//             type: HttpMethodType.post,
-//             expect: 201,
-//             auth: AuthType.admin,
-//             send: createReq
-//         })
-//         .then(async (resp) => {
-//             // console.dir(resp.body);
-//             await verifySuccessBody(resp.body, async (data: any) => {
-//                 should().exist(data.mechanical_treatment_id);
-//                 should().exist(data.species);
-//                 should().exist(data.speciesAgency);
-//                 should().exist(data.mechanicalMethod);
-//                 should().exist(data.mechanicalDisposalMethod);
-//                 should().exist(data.soilDisturbance);
-//                 should().exist(data.rootRemoval);
-//                 should().exist(data.issue);
-//                 should().exist(data.providerContractor);
-//                 expect(data.observation.observation_id).to.be.equal(create.observation.observation_id);
-//                 expect(data.species.species_id)
-//                 .to.be.equal(create.species.species_id);
-//                 expect(data.speciesAgency.species_agency_code_id)
-//                 .to.be.equal(create.speciesAgency.species_agency_code_id);
-//                 expect(data.mechanicalMethod.mechanical_method_code_id)
-//                 .to.be.equal(create.mechanicalMethod.mechanical_method_code_id);
-//                 expect(data.mechanicalDisposalMethod.mechanical_disposal_method_code_id)
-//                 .to.be.equal(create.mechanicalDisposalMethod.mechanical_disposal_method_code_id);
-//                 expect(data.soilDisturbance.mechanical_soil_disturbance_code_id).to.be.equal(create.soilDisturbance.mechanical_soil_disturbance_code_id);
-//                 expect(data.rootRemoval.mechanical_root_removal_code_id)
-//                 .to.be.equal(create.rootRemoval.mechanical_root_removal_code_id);
-//                 expect(data.issue.mechanical_treatment_issue_code_id)
-//                 .to.be.equal(create.issue.mechanical_treatment_issue_code_id);
-//                 expect(data.providerContractor.treatment_provider_contractor_id)
-//                 .to.be.equal(create.providerContractor.treatment_provider_contractor_id);
-//                 expect(data.date).to.be.equal(create.date);
-//                 await MechanicalTreatmentController.shared.removeById(data.mechanical_treatment_id);
-//             });
-//             await ObservationController.shared.remove(create.observation);
-//         });
-//     });
+    it('should create mechanical monitoring  for {admin}', async () => {
+        const create = await mechanicalMonitorCreateSpecFactory();
+        const createReq = RequestFactory<MechanicalMonitorSpec>(create, {
+            schema: MechanicalMonitorSchema.shared
+        });
+        await testRequest(SharedExpressApp.app , {
+            url: '/api/monitor/mechanical/',
+            type: HttpMethodType.post,
+            expect: 201,
+            auth: AuthType.admin,
+            send: createReq
+        })
+        .then(async (resp) => {
+            // console.dir(resp.body);
+            await verifySuccessBody(resp.body, async (data: any) => {
+                should().exist(data.mechanical_monitor_id);
+                should().exist(data.efficacy);
+                should().exist(data.observerFirstName);
+                should().exist(data.observerLastName);
+                should().exist(data.timestamp);
+                should().exist(data.paperFileID);
+                should().exist(data.speciesAgency);
+                should().exist(data.mechanicalTreatmentID);
 
-//     it('should not create mechanical treatment for {admin}: missing * fields', async () => {
-//         const create = await mechanicalTreatmentCreateSpecFactory();
-//         const createReq = RequestFactory<MechanicalTreatmentSpec>(create, {
-//             schema: MechanicalTreatmentSchema.shared
-//         });
-//         // Removing some required fields
-//         delete (createReq.longitude);
-//         delete (createReq.applicatorLastName);
-//         await testRequest(SharedExpressApp.app , {
-//             url: '/api/treatment/mechanical/',
-//             type: HttpMethodType.post,
-//             expect: 422,
-//             auth: AuthType.admin,
-//             send: createReq
-//         })
-//         .then(async (resp) => {
-//             await verifyErrorBody(resp.body);
-//             await ObservationController.shared.remove(create.observation);
-//         });
-//     });
+                expect(data.efficacy.efficacy_code_id).to.be.equal(create.efficacy.efficacy_code_id);
+                expect(data.observerFirstName).to.be.equal(create.observerFirstName);
+                expect(data.observerLastName).to.be.equal(create.observerLastName);
+                expect(data.timestamp).to.be.equal(create.timestamp);
+                expect(data.paperFileID).to.be.equal(create.paperFileID);
+                expect(data.speciesAgency.species_agency_code_id)
+                .to.be.equal(create.speciesAgency.species_agency_code_id);
+                expect(data.mechanicalTreatmentID.mechanical_treatment_id).to.be.equal(create.mechanicalTreatmentID.mechanical_treatment_id);
 
-//     it('should not create mechanical treatment for {viewer}', async () => {
-//         const create = await mechanicalTreatmentCreateSpecFactory();
-//         const createReq = RequestFactory<MechanicalTreatmentSpec>(create, {
-//             schema: MechanicalTreatmentSchema.shared
-//         });
-//         await testRequest(SharedExpressApp.app , {
-//             url: '/api/treatment/mechanical/',
-//             type: HttpMethodType.post,
-//             expect: 401,
-//             auth: AuthType.viewer,
-//             send: createReq
-//         })
-//         .then(async (resp) => {
-//             await verifyErrorBody(resp.body);
-//             await Destroyer(MechanicalTreatmentController.shared)(create, true);
-//         });
-//     });
+                await MechanicalMonitorController.shared.removeById(data.mechanical_monitor_id);
+            });
+        });
+    });
 
-//     it('should fetch mechanical treatments {all} for any user', async () => {
-//         const mt = await mechanicalTreatmentFactory();
-//         await testRequest(SharedExpressApp.app , {
-//             url: `/api/treatment/mechanical/`,
-//             type: HttpMethodType.get,
-//             expect: 200,
-//             auth: AuthType.viewer
-//         })
-//         .then(async (resp) => {
-//             await verifySuccessBody(resp.body, async data => {
-//                 expect(data.length).to.be.greaterThan(0);
-//                 /*const filtered = data.filter( (obj: any) => obj.mechanical_treatment_id === mt.mechanical_treatment_id);
-//                 expect(filtered.length === 1).to.be.equal(true);*/
-//             });
-//             await destroyMechanicalTreatment(mt);
-//         });
-//     });
+    it('should not create mechanical monitor for {admin}: missing * fields', async () => {
+        const create = await mechanicalMonitorCreateSpecFactory();
+        const createReq = RequestFactory<MechanicalMonitorSpec>(create, {
+            schema: MechanicalMonitorSchema.shared
+        });
+        // Removing some required fields
+        delete (createReq.applicatorLastName);
+        delete (createReq.efficacy);
+        await testRequest(SharedExpressApp.app , {
+            url: '/api/monitor/mechanical/',
+            type: HttpMethodType.post,
+            expect: 422,
+            auth: AuthType.admin,
+            send: createReq
+        })
+        .then(async (resp) => {
+            await verifyErrorBody(resp.body);
+        });
+    });
 
-//     it('should update mechanical treatment for {admin}', async () => {
-//         const mt = await mechanicalTreatmentFactory();
-//         const create = await mechanicalTreatmentCreateSpecFactory();
-//         delete create.species;
-//         await ObservationController.shared.remove(create.observation);
-//         delete create.observation;
-//         delete create.applicatorLastName;
-//         const updateReq = RequestFactory<MechanicalTreatmentUpdateSpec>(create, {
-//             schema: MechanicalTreatmentSchema.shared
-//         });
-//         await testRequest(SharedExpressApp.app , {
-//             url: `/api/treatment/mechanical/${mt.mechanical_treatment_id}`,
-//             type: HttpMethodType.put,
-//             expect: 200,
-//             auth: AuthType.admin,
-//             send: updateReq
-//         })
-//         .then(async (resp) => {
-//             await verifySuccessBody(resp.body, async (data: any) => {
-//                 should().exist(data.mechanical_treatment_id);
-//                 expect(data.mechanical_treatment_id).to.be.equal(mt.mechanical_treatment_id);
-//                 expect(data.observation.observation_id).to.be.equal(mt.observation.observation_id);
-//                 expect(data.species.species_id).to.be.equal(mt.species.species_id);
-//                 expect(data.speciesAgency.species_agency_code_id).to.be.equal(updateReq.speciesAgency);
-//                 expect(data.mechanicalMethod.mechanical_method_code_id).to.be.equal(updateReq.mechanicalMethod);
-//                 expect(data.date).to.be.equal(create.date);
-//             });
-//             await destroyMechanicalTreatment(mt);
-//         });
-//     });
+    it('should not create mechanical monitor for {viewer}', async () => {
+        const create = await mechanicalMonitorCreateSpecFactory();
+        const createReq = RequestFactory<MechanicalMonitorSpec>(create, {
+            schema: MechanicalMonitorSchema.shared
+        });
+        await testRequest(SharedExpressApp.app , {
+            url: '/api/monitor/mechanical/',
+            type: HttpMethodType.post,
+            expect: 401,
+            auth: AuthType.viewer,
+            send: createReq
+        })
+        .then(async (resp) => {
+            await verifyErrorBody(resp.body);
+            await Destroyer(MechanicalMonitorController.shared)(create, true);
+        });
+    });
 
-//     it('should fetch mechanical treatments {single} for any user', async () => {
-//         let mt: any;
-//         try {
-//             mt = await mechanicalTreatmentFactory();
-//         } catch (excp) {
-//             console.log(`${excp}`);
-//         }
+    it('should fetch mechanical monitoring records {all} for any user', async () => {
+        const monitorRecord = await mechanicalMonitorFactory();
+        await testRequest(SharedExpressApp.app , {
+            url: `/api/monitor/mechanical/`,
+            type: HttpMethodType.get,
+            expect: 200,
+            auth: AuthType.viewer
+        })
+        .then(async (resp) => {
+            await verifySuccessBody(resp.body, async data => {
+                expect(data.length).to.be.greaterThan(0);
+            });
+            await destroyMechanicalMonitor(monitorRecord);
+        });
+    });
 
-//         await request(SharedExpressApp.app)
-//             .get(`/api/treatment/mechanical/${mt.mechanical_treatment_id}`)
-//             .set('Authorization', `Bearer ${viewerToken()}`)
-//             .expect(200)
-//             .then(async (resp) => {
-//                 await verifySuccessBody(resp.body, async data => {
-//                     expect(data.mechanical_treatment_id).to.be.equal(mt.mechanical_treatment_id);
-//                 });
-//                 await destroyMechanicalTreatment(mt);
-//             });
-//     });
+    it('should update mechanical monitoring record for {admin}', async () => {
+        const monitorRecord = await mechanicalMonitorFactory();
+        const updateReq = RequestFactory<MechanicalMonitorUpdateSpec>(monitorRecord, {
+            schema: MechanicalMonitorSchema.shared
+        });
+        await testRequest(SharedExpressApp.app , {
+            url: `/api/monitor/mechanical/${monitorRecord.mechanical_monitor_id}`,
+            type: HttpMethodType.put,
+            expect: 200,
+            auth: AuthType.admin,
+            send: updateReq
+        })
+        .then(async (resp) => {
+            await verifySuccessBody(resp.body, async (data: any) => {
+                should().exist(data.mechanical_monitor_id);
+                expect(data.mechanical_monitor_id).to.be.equal(monitorRecord.mechanical_monitor_id);
+                expect(data.mechanicalTreatmentID.mechanical_treatment_id).to.be.equal(monitorRecord.mechanicalTreatmentID.mechanical_treatment_id);
+                expect(data.speciesAgency.species_agency_code_id).to.be.equal(updateReq.speciesAgency);
+                expect(data.timestamp).to.be.equal(monitorRecord.timestamp);
+            });
+            await destroyMechanicalMonitor(monitorRecord);
+        });
+    });
 
-//     it('should fetch mechanical treatment resource config', async () => {
-//         await testRequest(SharedExpressApp.app, {
-//             url: `/api/treatment/mechanical/config`,
-//             type: HttpMethodType.get,
-//             expect: 200,
-//             auth: AuthType.viewer
-//         })
-//         .then(async (resp) => {
-//             verifySuccessBody(resp.body, async data => {
-//                 should().exist(data.idKey);
-//                 should().exist(data.layout);
-//                 should().exist(data.meta);
-//                 should().exist(data.fields);
-//                 should().exist(data.schemaName);
-//                 should().exist(data.modelName);
-//                 should().exist(data.description);
-//             });
-//         });
-//     });
-// });
+    it('should fetch mechanical monitoring record {single} for any user', async () => {
+        let monitorRecord: any;
+        try {
+            monitorRecord = await mechanicalMonitorFactory();
+        } catch (excp) {
+            console.log(`${excp}`);
+        }
+
+        await request(SharedExpressApp.app)
+            .get(`/api/monitor/mechanical/${monitorRecord.mechanical_monitor_id}`)
+            .set('Authorization', `Bearer ${viewerToken()}`)
+            .expect(200)
+            .then(async (resp) => {
+                await verifySuccessBody(resp.body, async data => {
+                    expect(data.mechanical_monitor_id).to.be.equal(monitorRecord.mechanical_monitor_id);
+                });
+                await destroyMechanicalMonitor(monitorRecord);
+            });
+    });
+
+    it('should fetch mechanical monitor resource config', async () => {
+        await testRequest(SharedExpressApp.app, {
+            url: `/api/monitor/mechanical/config`,
+            type: HttpMethodType.get,
+            expect: 200,
+            auth: AuthType.viewer
+        })
+        .then(async (resp) => {
+            verifySuccessBody(resp.body, async data => {
+                should().exist(data.idKey);
+                should().exist(data.layout);
+                should().exist(data.meta);
+                should().exist(data.fields);
+                should().exist(data.schemaName);
+                should().exist(data.modelName);
+                should().exist(data.description);
+            });
+        });
+    });
+});
