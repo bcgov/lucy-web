7494916eb7689e150a56368cd904f1555e71bbd3
rajfreshworks <raj@freshworks.io>
2020-02-07 12:52:00 -0800

    user account status change feature added

--- app/lucy/src/app/components/Routes/admin-tools/admin-tools.component.ts
+++ app/lucy/src/app/components/Routes/admin-tools/admin-tools.component.ts
@@ -16,13 +16,14 @@
  * 	Created by Amir Shayegh on 2019-10-23.
  */
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { MatTableDataSource, MatSelectChange } from '@angular/material';
+import { MatTableDataSource, MatSelectChange, MatSlideToggleChange } from '@angular/material';
 import { RolesService } from 'src/app/services/roles.service';
 import { AdminService } from 'src/app/services/admin.service';
 import { LoadingService } from 'src/app/services/loading.service';
 import { UserService } from 'src/app/services/user.service';
 import { ExportService, ExportType } from 'src/app/services/export/export.service';
 import { AlertService } from 'src/app/services/alert.service';
+import { ToastService, ToastIconType } from 'src/app/services/toast/toast.service';
 import { AccessRequest, AccessRequestTableData } from 'src/app/models/AccessRequest';
 import { User, UserTableData } from 'src/app/models';
 import { Role } from 'src/app/models/Role';
@@ -58,9 +59,18 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     return this.allUsers.length !== 0;
   }
 
+  get hasExportData(): boolean {
+    return this.numberOfDataInInspectAppToExport > 0;
+  }
+
   get getUserRole(): string {
     if (!this.selectedUser) return '';
     return this.userService.getUserAccessCode(this.selectedUser).role;
+  }
+
+  get getUserStatus(): boolean {
+    if (!this.selectedUser) return false;
+    return this.selectedUser.accountStatus === 1;
   }
 
   showUserPopper(user: User): boolean {
@@ -78,7 +88,8 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     private loadingService: LoadingService,
     private exportService: ExportService,
     private userService: UserService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private toastService: ToastService
   ) { }
 
   ngOnInit() { }
@@ -162,17 +173,30 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     this.usersDataSource = new MatTableDataSource<UserTableData>(users);
   }
 
-  private changeUserRole(data: MatSelectChange) {
+  public changeUserRole(data: MatSelectChange) {
     const selectedRole = this.activeRoles.find(activeRole => activeRole.role === data.value);
-    this.admin.changeUserRole(this.selectedUser, selectedRole).then((response) => {
+    this.admin.changeUserRole(this.selectedUser, selectedRole).then(async (response) => {
       if (response.success) {
-        this.getAllUsers()
-        this.alertService.show(`Success`,
-         `${this.selectedUser.firstName}'s role change to ${selectedRole.role}.`, null);
+        this.selectedUser = response.response;
+        await this.getAllUsers();
+        this.toastService.show(`${this.selectedUser.firstName}'s role change to ${selectedRole.role}.`, ToastIconType.success);
       } else {
-        this.alertService.show(`Failed`, `Could not change user role.`, null);
+        this.toastService.show('Failed, Could not change user role', ToastIconType.fail);
       }
     })
+  }
+
+  public changeUserStatus(toggle: MatSlideToggleChange) {
+    const accountStatus = toggle.checked ? 1 : 0;
+    this.admin.changeUserAccountStatus(this.selectedUser, accountStatus).then(async (response) => {
+      if(response.success) {
+        this.selectedUser = response.response;
+        await this.getAllUsers();
+        this.toastService.show(`${this.selectedUser.firstName}'s account status had been changed.`, ToastIconType.success);
+      } else {
+        this.toastService.show(`Failed, Could not change the status of ${this.selectedUser.firstName}'s account.`, ToastIconType.fail);
+      }
+    });
   }
 
   private async getNumberOfDataInInspectAppToExport() {
