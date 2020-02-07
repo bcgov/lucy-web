e4edb10b5e6c5fe86ee022bac4123ee5e2aa9609
rajfreshworks <raj@freshworks.io>
2020-02-07 10:11:23 -0800

    popper added to users table

--- app/lucy/src/app/components/Routes/admin-tools/admin-tools.component.ts
+++ app/lucy/src/app/components/Routes/admin-tools/admin-tools.component.ts
@@ -16,12 +16,13 @@
  * 	Created by Amir Shayegh on 2019-10-23.
  */
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatSelectChange } from '@angular/material';
 import { RolesService } from 'src/app/services/roles.service';
 import { AdminService } from 'src/app/services/admin.service';
 import { LoadingService } from 'src/app/services/loading.service';
 import { UserService } from 'src/app/services/user.service';
 import { ExportService, ExportType } from 'src/app/services/export/export.service';
+import { AlertService } from 'src/app/services/alert.service';
 import { AccessRequest, AccessRequestTableData } from 'src/app/models/AccessRequest';
 import { User, UserTableData } from 'src/app/models';
 import { Role } from 'src/app/models/Role';
@@ -36,6 +37,8 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
   public requests: AccessRequest[] = [];
   public allUsers: User[] = [];
   public activeRoles: Role[] = [];
+  public selectedUser: User;
+  public selectedRequestUser: AccessRequest;
 
   public focusedAccessRequest: AccessRequest;
 
@@ -55,12 +58,23 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     return this.allUsers.length !== 0;
   }
 
+  showUserPopper(user: User): boolean {
+    if (!user || !this.selectedUser) return false;
+    return this.selectedUser.user_id === user.user_id;
+  }
+
+  get getUserRole(): string {
+    if (!this.selectedUser) return '';
+    return this.userService.getUserAccessCode(this.selectedUser).role;
+  }
+
   constructor(
     private roles: RolesService,
     private admin: AdminService,
     private loadingService: LoadingService,
     private exportService: ExportService,
-    private userService: UserService
+    private userService: UserService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit() { }
@@ -112,14 +126,6 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     this.requestUsersDataSource = new MatTableDataSource<AccessRequestTableData>(reqUsers);
   }
 
-  private async getNumberOfDataInInspectAppToExport() {
-    this.exportService.getInspectAppExportData(ExportType.WatercraftRiskAssessment).then((data) => {
-      if (data){
-        this.numberOfDataInInspectAppToExport = data.length
-      }
-    });
-  }
-
   private async getAllUsers() {
     this.loadingService.add();
     this.admin.getAllUsers().then(async (value) => {
@@ -134,7 +140,7 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
     if (allUsers.length === 0) return;
 
     allUsers.forEach(user => {
-      const { firstName, lastName, email, preferredUsername } = user;
+      const { firstName, lastName, email, preferredUsername, user_id } = user;
 
       const username = preferredUsername;
       const name = firstName + ' ' + lastName; 
@@ -144,11 +150,33 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
         username,
         name,
         email,
-        role
+        role,
+        user_id
       });
     });
 
     this.usersDataSource = new MatTableDataSource<UserTableData>(users);
+  }
+
+  private changeUserRole(data: MatSelectChange) {
+    const selectedRole = this.activeRoles.find(activeRole => activeRole.role === data.value);
+    this.admin.changeUserRole(this.selectedUser, selectedRole).then((response) => {
+      if (response.success) {
+        this.getAllUsers()
+        this.alertService.show(`Success`,
+         `${this.selectedUser.firstName}'s role change to ${selectedRole.role}.`, null);
+      } else {
+        this.alertService.show(`Failed`, `Could not change user role.`, null);
+      }
+    })
+  }
+
+  private async getNumberOfDataInInspectAppToExport() {
+    this.exportService.getInspectAppExportData(ExportType.WatercraftRiskAssessment).then((data) => {
+      if (data){
+        this.numberOfDataInInspectAppToExport = data.length
+      }
+    });
   }
 
   private async getAllRoles() {
@@ -165,5 +193,13 @@ export class AdminToolsComponent implements OnInit, AfterViewInit {
 
   public async export() {
     this.exportService.exportCSV(ExportType.WatercraftRiskAssessment);
+  }
+
+  public onUserAction(item: UserTableData) {
+    const { user_id } = item;
+    if (!user_id) return;
+
+    const selectedUser = this.allUsers.find(user => user.user_id === user_id);
+    this.selectedUser=selectedUser;
   }
 }
