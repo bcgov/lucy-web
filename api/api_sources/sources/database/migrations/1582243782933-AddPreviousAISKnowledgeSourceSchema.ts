77174760107e91d64c130817591785fdf07e6e33
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-20 16:14:27 -0800

    Add schema DB migration files

---
+++ api/api_sources/sources/database/migrations/1582243782933-AddPreviousAISKnowledgeSourceSchema.ts
@@ -0,0 +1,30 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+import { AppDBMigrator } from '../applicationSchemaInterface';
+import { PreviousAISKnowledgeSourceSchema } from '../database-schema';
+
+export class AddPreviousAISKnowledgeSourceSchema1582243782933 extends AppDBMigrator implements MigrationInterface {
+
+    previousAISKnowledgeSourceSchema: PreviousAISKnowledgeSourceSchema;
+
+    /**
+     * Setup
+     */
+    setup() {
+        this.previousAISKnowledgeSourceSchema = new PreviousAISKnowledgeSourceSchema();
+        this.addSchemaInitVersion(this.previousAISKnowledgeSourceSchema);
+    }
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        // Start Log
+        this.log('[START]', 'UP');
+        await this.runQuerySqlFiles(this.upMigrations(), queryRunner);
+        this.log('[END]', 'UP');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        this.log('[START]', 'DOWN');
+        await queryRunner.query(this.previousAISKnowledgeSourceSchema.dropTable());
+        this.log('[END]', 'DOWN');
+    }
+
+}
