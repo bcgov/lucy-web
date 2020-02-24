73006b89d51fe260bf754015c8ab03592415b583
fwpushan <pushan@freshworks.io>
2020-02-25 00:10:19 +0530

    Adding Other Option to Journey Details --------------------------------------- 1. Add new field to WatercraftJourney 2. Update DB Migration/Schema 3. Update model + test 4. Add some error failure for db migration process ----------------------

--- api/api_sources/schema-migration-sql/WatercraftJourneySchema/WatercraftJourneySchema.sql
+++ api/api_sources/schema-migration-sql/WatercraftJourneySchema/WatercraftJourneySchema.sql
@@ -5,6 +5,7 @@ CREATE TABLE watercraft_journey ();
 ALTER TABLE watercraft_journey ADD COLUMN watercraft_journey_id SERIAL PRIMARY KEY;
 ALTER TABLE watercraft_journey ADD COLUMN journey_type INT NOT NULL DEFAULT 0;
 ALTER TABLE watercraft_journey ADD COLUMN number_of_days_out INT NULL;
+ALTER TABLE watercraft_journey ADD COLUMN other_water_body_detail VARCHAR(300) NULL;
 ALTER TABLE watercraft_journey ADD COLUMN watercraft_risk_assessment_id INT NULL REFERENCES watercraft_risk_assessment(watercraft_risk_assessment_id) ON DELETE SET NULL;
 ALTER TABLE watercraft_journey ADD COLUMN water_body_id INT NULL REFERENCES water_body(water_body_id) ON DELETE SET NULL;
 
@@ -17,6 +18,7 @@ COMMENT ON TABLE watercraft_journey IS 'Joining table for water-body details and
 COMMENT ON COLUMN watercraft_journey.watercraft_journey_id IS 'Auto generated sequential primary key column.';
 COMMENT ON COLUMN watercraft_journey.journey_type IS 'Journey type of the associated regarding water body. i.e Previous (0) and next (1)';
 COMMENT ON COLUMN watercraft_journey.number_of_days_out IS 'Number of days out of water';
+COMMENT ON COLUMN watercraft_journey.other_water_body_detail IS 'Details of not listed water body. This is an optional field. User can provide a brief description of water-body which is not listed in application water body list.';
 COMMENT ON COLUMN watercraft_journey.watercraft_risk_assessment_id IS 'Foreign key reference to Watercraft risk assessment table';
 COMMENT ON COLUMN watercraft_journey.water_body_id IS 'Foreign key reference to Water body detail table';
 
