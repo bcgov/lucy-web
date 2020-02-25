9e7352a7447357b6b92512bce2aa1efcb806a7c0
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-25 11:32:20 -0800

    Refactor MusselAppDB migration file per Pushan's review

--- api/api_sources/sources/database/migrations/1572894977602-MusselAppDatabase.ts
+++ api/api_sources/sources/database/migrations/1572894977602-MusselAppDatabase.ts
@@ -1,18 +1,29 @@
 import {MigrationInterface, QueryRunner} from 'typeorm';
-import { WatercraftRiskAssessmentSchema, ObserverWorkflowSchema, HighRiskAssessmentSchema } from '../database-schema';
+import { WatercraftRiskAssessmentSchema,
+         ObserverWorkflowSchema,
+         HighRiskAssessmentSchema,
+         AdultMusselsLocationSchema,
+         PreviousAISKnowledgeSourceSchema,
+         PreviousInspectionSourceSchema } from '../database-schema';
 import { AppDBMigrator } from '../applicationSchemaInterface';
 
 
-export class CreateMusselAppDatabase1572582003161 extends AppDBMigrator implements MigrationInterface {
+export class MusselAppDatabase1572894977602 extends AppDBMigrator implements MigrationInterface {
 
     waterCraftRiskAssessmentSchema: WatercraftRiskAssessmentSchema;
     observerWorkflowSchema: ObserverWorkflowSchema;
     highRiskAssessmentSchema: HighRiskAssessmentSchema;
+    adultMusselsLocationSchema: AdultMusselsLocationSchema;
+    previousAISKnowledgeSourceSchema: PreviousAISKnowledgeSourceSchema;
+    previousInspectionSourceSchema: PreviousInspectionSourceSchema;
     /**
      * Setup
      */
     setup() {
         // Adding Water craft risk assessment init schema to migrator
+        this.adultMusselsLocationSchema = new AdultMusselsLocationSchema();
+        this.previousAISKnowledgeSourceSchema = new PreviousAISKnowledgeSourceSchema();
+        this.previousInspectionSourceSchema = new PreviousInspectionSourceSchema();
         this.waterCraftRiskAssessmentSchema = new WatercraftRiskAssessmentSchema();
         this.observerWorkflowSchema = new ObserverWorkflowSchema();
         this.highRiskAssessmentSchema = new HighRiskAssessmentSchema();
@@ -20,6 +31,12 @@ export class CreateMusselAppDatabase1572582003161 extends AppDBMigrator implemen
         // Creating table
         this.addSchemaInitVersion(this.observerWorkflowSchema);
         this.addSchemaInitVersion(this.highRiskAssessmentSchema);
+        this.addSchemaInitVersion(this.adultMusselsLocationSchema);
+        this.addDataImportMigration(this.adultMusselsLocationSchema, 'init');
+        this.addSchemaInitVersion(this.previousAISKnowledgeSourceSchema);
+        this.addDataImportMigration(this.previousAISKnowledgeSourceSchema, 'init');
+        this.addSchemaInitVersion(this.previousInspectionSourceSchema);
+        this.addDataImportMigration(this.previousInspectionSourceSchema, 'init');
         this.addSchemaInitVersion(this.waterCraftRiskAssessmentSchema);
         // Alter table with version
         this.addSchemaVersion(this.waterCraftRiskAssessmentSchema, 'workflow');
@@ -39,6 +56,9 @@ export class CreateMusselAppDatabase1572582003161 extends AppDBMigrator implemen
         await queryRunner.query(this.waterCraftRiskAssessmentSchema.dropTable());
         await queryRunner.query(this.highRiskAssessmentSchema.dropTable());
         await queryRunner.query(this.observerWorkflowSchema.dropTable());
+        await queryRunner.query(this.adultMusselsLocationSchema.dropTable());
+        await queryRunner.query(this.previousInspectionSourceSchema.dropTable());
+        await queryRunner.query(this.previousAISKnowledgeSourceSchema.dropTable());
     }
 
 }
