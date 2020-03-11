263df0652b7f9b6d0862281a5f0be88f9f5cb341
Andrea Williams <andrea.williams@gov.bc.ca>
2020-03-11 13:08:58 -0700

    Add lat/long values to wapoint-text-entry

--- app/lucy/src/app/components/base-form/waypoint/waypoint-text-entry/waypoint-text-entry.component.ts
+++ app/lucy/src/app/components/base-form/waypoint/waypoint-text-entry/waypoint-text-entry.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormMode } from 'src/app/models';
+import { ValidationService } from 'src/app/services/validation.service';
+
 
 @Component({
   selector: 'app-waypoint-text-entry',
@@ -7,20 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaypointTextEntryComponent implements OnInit {
 
-   lat;
-   long; 
+   lat: string;
+   long: string;
+   validation: ValidationService;
 
-  constructor() { }
+  ///// Form Mode
+  private _mode: FormMode = FormMode.View;
+  get mode(): FormMode {
+    return this._mode;
+  }
+
+  @Input() set mode(mode: FormMode) {
+    this._mode = mode;
+  }
+
+  constructor(
+  ) { }
 
   ngOnInit() {
+    this.validation = new ValidationService();
   }
 
-  latitudeChanged() {
-
+  latChanged(value: string) {
+    if ((this.validation.isValidLatitude(value)) || (value === ``)) {
+      this.lat = value;
+    }
   }
 
-  longitudeChanged() {
-
+  longChanged(value: string) {
+    if ((this.validation.isValidLongitude(value)) || (value === ``)) {
+      this.long = value;
+    }
   }
 
 }
