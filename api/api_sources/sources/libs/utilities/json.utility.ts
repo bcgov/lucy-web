ab08d89aea6cd0c510ce736065e08bc06db0cd34
fwpushan <pushan@freshworks.io>
2020-04-27 20:00:24 +0530

    Build Issue: Fix lint error on file

--- api/api_sources/sources/libs/utilities/json.utility.ts
+++ api/api_sources/sources/libs/utilities/json.utility.ts
@@ -1,4 +1,27 @@
-import { capitalize } from "./helpers.utilities";
+/*
+ * Copyright © 2019 Province of British Columbia
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * **
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * **
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * File: json.utility.ts
+ * Project: lucy
+ * File Created: Monday, 29th Jan 2020 11:05:18 am
+ * Author: Pushan
+ * -----
+ * Last Modified: Monday, 27th April 2020 7:57:29 pm
+ * Modified By: Pushan
+ * -----
+ */
+
+import { capitalize } from './helpers.utilities';
 
 /**
  * Utility class for json or object
