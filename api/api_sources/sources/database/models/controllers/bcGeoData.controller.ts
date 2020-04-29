219bfbbbb59bfd44803c08dd3c9a9aedd27318fb
Andrea Williams <andrea.williams@gov.bc.ca>
2020-04-29 11:04:43 -0700

    Fix BCGeoData route controller; get wells with bbox works now

--- api/api_sources/sources/database/models/controllers/bcGeoData.controller.ts
+++
@@ -1,8 +0,0 @@
-import { BCGeoJSON } from '../bcGeoData';
-import { RecordController } from '../generic.data.models';
-
-export class BCGeoDataController extends RecordController<BCGeoJSON> {
-    public static get shared(): BCGeoDataController {
-        return this.sharedInstance<BCGeoJSON>(BCGeoJSON) as BCGeoDataController;
-    }
-}
