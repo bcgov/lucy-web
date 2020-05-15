f72cabd8f2b238e6b82c127bff12a517155c00cc
rajfreshworks <raj@freshworks.io>
2020-05-15 13:16:54 -0700

    schema changes

--- api/api_sources/schema-migration-sql/AnimalObservationSchema/AnimalObservationSchema-spaceGeom-20191128.down.sql
+++
@@ -1,7 +0,0 @@
--- ## Reverting table: animal_observation
--- ## Version: spaceGeom
--- ## Info: None
--- ## Removing New Columns ## --
-ALTER TABLE animal_observation DROP COLUMN IF EXISTS space_geom_id;
-
--- ## Updating animal_observation ## --
