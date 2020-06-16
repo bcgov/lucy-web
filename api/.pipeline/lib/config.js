3099a6eece6c597b17e44c7222bb01d85775884d
fwpushan <pushan@freshworks.io>
2020-06-16 20:59:47 +0530

    Pipeline: Fix git option issues

--- api/.pipeline/lib/config.js
+++ api/.pipeline/lib/config.js
@@ -1,6 +1,6 @@
 'use strict';
 const options= require('pipeline-cli').Util.parseArguments();
-console.dir(options);
+
 const config = require('../../../.config/config.json');
 const changeId = options.pr || `${Math.floor((Date.now() * 1000)) / 60.0}`; //aka pull-request or brach to process
 const version = config.version || '1.0.0';
@@ -18,6 +18,27 @@ const isProduction = () => options.env === 'prod';
 const defaultHost = 'invasivebc-8ecbmv-api.pathfinder.gov.bc.ca';
 const branch = isStaticDeployment() && !isProduction() ? options.branch : undefined;
 const tag = isStaticDeployment() && !isProduction() ? `build-${version}-${changeId}-${branch}` : `build-${version}-${changeId}`;
+
+const processOptions = (options) => {
+  const result = { ...options };
+  // Check git
+  if (!result.git.url.includes('.git')) {
+    result.git.url = `${result.git.url}.git`
+  }
+  if (!result.git.http_url.includes('.git')) {
+    result.git.http_url = `${result.git.http_url}.git`
+  }
+
+  // Fixing repo
+  if (result.git.repository.includes('/')) {
+    const last = result.git.repository.split('/').pop();
+    const final = last.split('.')[0];
+    result.git.repository = final;
+  }
+  return result;
+};
+
+const finalOptions = processOptions(options);
 
 const phases = {
   build: {
@@ -89,4 +110,4 @@ process.on('unhandledRejection', (reason) => {
   process.exit(1);
 });
 
-module.exports = exports = {phases, options, staticBranches};
+module.exports = exports = {phases, finalOptions, staticBranches};
