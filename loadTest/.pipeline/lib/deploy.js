8722764dc5be68488fbc9897059fabc941167009
fwpushan <pushan@freshworks.io>
2020-06-20 02:24:13 +0530

    Fixed ----- 1. Openshift dir name issue 2. Context dir name -----

--- loadTest/.pipeline/lib/deploy.js
+++ loadTest/.pipeline/lib/deploy.js
@@ -1,33 +1,48 @@
 'use strict';
 const {OpenShiftClientX} = require('pipeline-cli')
 const path = require('path');
+const wait = require('./wait');
+const checkAndClean = require('./checkAndClean');
 
-module.exports = (settings)=>{
+module.exports = (settings) => {
   const phases = settings.phases
   const options= settings.options
-  const phase=options.env
+  const phase = options.env
   const changeId = phases[phase].changeId
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
+  const oc= new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
   const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
   var objects = []
+
   // The deployment of your cool app goes here ▼▼▼
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/app.dc.yaml`, {
+  const instance = `${phases[phase].name}-${changeId}`;
+  const image = `${phases[phase].name}:${phases[phase].tag}-setup`;
+
+  // Get API image stream
+  const data = oc.get(`istag/${image}`) || [];
+  if (data.length === 0) {
+    console.log('Unable to fetch API imag ref');
+    process.exit(0);
+  }
+  const imageStream = data[0];
+  const podName = `${phases[phase].name}${phases[phase].suffix}-test`;
+
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/test.pod.yaml`, {
     'param':{
-      'NAME': phases[phase].name,
+      'NAME': podName,
       'SUFFIX': phases[phase].suffix,
       'VERSION': phases[phase].tag,
-      'HOST': phases[phase].host,
-      'CHANGE_ID':  phases.build.changeId || changeId,
-      'API_HOST': phases[phase].apiHost,
-      'ENVIRONMENT':  phases[phase].env || 'dev',
-      'SSO_URL': phases[phase].sso.url,
-      'SSO_CLIENT_ID': phases[phase].sso.clientId,
-      'SSO_REALM': phases[phase].sso.realm,
-      'REPLICAS': phases[phase].replicas || 1,
-      'REPLICA_MAX': phases[phase].maxReplicas || 1
+      'CHANGE_ID': phases[phase].changeId,
+      'ENVIRONMENT': phases[phase].env || 'dev',
+      'DB_SERVICE_NAME': `${phases[phase].name}-postgresql${phases[phase].suffix}`,
+      'IMAGE': imageStream.image.dockerImageReference,
+      'CERTIFICATE_URL': 'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/dfmlcg7z/protocol/openid-connect/certs',
+      'DB_MIGRATION_TYPE': phases[phase].migrationInfo.type,
+      'DB_CLEAN_UP': phases[phase].migrationInfo.cleanup,
+      'DB_SEED': phases[phase].migrationInfo.dbSeed
     }
   }))
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
+  checkAndClean(`pod/${podName}`, oc);
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, instance)
   oc.applyAndDeploy(objects, phases[phase].instance)
+  wait(`pod/${podName}`, settings, 35);
 }
