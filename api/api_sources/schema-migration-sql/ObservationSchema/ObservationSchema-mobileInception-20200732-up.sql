5b686d7ca0f7d0c86790286eb57f33a89e9759fc
Jamie Popkin <popkinj@littleearth.ca>
2020-08-04 15:46:19 -0700

    The remaining columns

--- api/api_sources/schema-migration-sql/ObservationSchema/ObservationSchema-mobileInception-20200732-up.sql
+++ api/api_sources/schema-migration-sql/ObservationSchema/ObservationSchema-mobileInception-20200732-up.sql
@@ -14,7 +14,7 @@ COMMENT ON COLUMN observation.hex_sub_id IS 'Calculated from the geometry (alber
 -- ## --
 
 -- ## Adding Column total_area on table observation
-ALTER TABLE observation ADD COLUMN total_area INTEGER NULL;
+ALTER TABLE observation ADD COLUMN total_area DOUBLE PRECISION NULL;
 COMMENT ON COLUMN observation.total_area IS 'Calculated from the geometry (square meters)';
 -- ## --
 
@@ -65,5 +65,77 @@ ALTER TABLE observation ADD COLUMN flowering BOOLEAN NOT NULL DEFAULT FALSE;
 COMMENT ON COLUMN observation.flowering IS 'Indicates the plant is flowering';
 -- ## --
 
+-- ## Adding Column bec_zone on table observation
+ALTER TABLE observation ADD COLUMN bec_zone VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.bec_zone IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column riso on table observation
+ALTER TABLE observation ADD COLUMN riso VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.riso IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column ipma on table observation
+ALTER TABLE observation ADD COLUMN ipma VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.ipma IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column ownership on table observation
+ALTER TABLE observation ADD COLUMN ownership VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.ownership IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column regional_district on table observation
+ALTER TABLE observation ADD COLUMN regional_district VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.regional_district IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column flnro_district on table observation
+ALTER TABLE observation ADD COLUMN flnro_district VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.flnro_district IS 'Look up from BC warehouse layer';
+-- ## --
+
+-- ## Adding Column moti_district on table observation
+ALTER TABLE observation ADD COLUMN moti_district VARCHAR(30) NULL;
+COMMENT ON COLUMN observation.moti_district IS 'Look up from BC warehouse layer';
+-- ## --
+
+
+-------------------------------- Geometry columns ------------------------------------
+
+-- ## Adding Column raw_longitude on table observation
+ALTER TABLE observation ADD COLUMN raw_longitude DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_longitude IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_latitude on table observation
+ALTER TABLE observation ADD COLUMN raw_latitude DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_latitude IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_utm_zone on table observation
+ALTER TABLE observation ADD COLUMN raw_utm_zone INTEGER NULL;
+COMMENT ON COLUMN observation.raw_utm_zone IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_utm_easting on table observation
+ALTER TABLE observation ADD COLUMN raw_utm_easting DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_utm_easting IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_utm_northing on table observation
+ALTER TABLE observation ADD COLUMN raw_utm_northing DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_utm_northing IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_albers_x on table observation
+ALTER TABLE observation ADD COLUMN raw_albers_x DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_albers_x IS 'Calculated from the geometry (square meters)';
+-- ## --
+
+-- ## Adding Column raw_albers_y on table observation
+ALTER TABLE observation ADD COLUMN raw_albers_y DOUBLE PRECISION NULL;
+COMMENT ON COLUMN observation.raw_albers_y IS 'Calculated from the geometry (square meters)';
+-- ## --
 
 -- ## Updating observation ## --
