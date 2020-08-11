48b3547412b31a48991b2ef7606dd850535050da
Jamie Popkin <popkinj@littleearth.ca>
2020-08-11 16:41:25 -0700

    doc

--- api-mobile/test/observation.sh
+++ api-mobile/test/observation.sh
@@ -1,3 +1,4 @@
+# TODO: Assign DB environment variables
 curl -X POST -d @./observation-fake.json "localhost:3002/api/activity" \
 --header "Content-Type: application/json" \
 --header 'Authorization: Bearer '${1} \
