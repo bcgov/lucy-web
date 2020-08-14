2be2d0bad3138c0e959bef8365b2506fcc5fcfd7
Mike Wells <micheal.w.wells@gmail.com>
2020-08-14 15:59:00 -0700

    stop using swagger-tools for request validation and use swagger-object-validator instead

--- api-mobile/app.ts
+++ api-mobile/app.ts
@@ -40,11 +40,12 @@ if (API_HOSTNAME !== 'localhost:3002') {
   swaggerConfig.schemes = ['https'];
 }
 
+
 swaggerTools.initializeMiddleware(swaggerConfig, async function (middleware) {
   // add base swagger route handling middleware
   app.use(middleware.swaggerMetadata());
 
-  app.use(middleware.swaggerValidator({ validateResponse: false }));
+//  app.use(middleware.swaggerValidator({ validateResponse: false }));
 
   // add swagger security for authenticated routes
   app.use(
