d2a9ed6fc0aff1134c79655b0d776276df8e7b41
Mike Wells <micheal.w.wells@gmail.com>
2020-08-17 15:41:30 -0700

    fix migration

--- api-mobile/src/database/migrations/20200811120312_stagingTable.ts
+++ api-mobile/src/database/migrations/20200811120312_stagingTable.ts
@@ -2,8 +2,9 @@ import Knex from 'knex';
 
 exports.up = async (knex: Knex) => {
 	await knex.raw(`
-    set search_path = invasivesbc,public;
     set schema 'invasivesbc';
+    set search_path = invasivesbc,public;
+    
 
     -- ### Creating Table: activity_incoming_data ### --
 
@@ -20,12 +21,12 @@ exports.up = async (knex: Knex) => {
     COMMENT ON COLUMN activity_incoming_data.version IS 'Indicative of the version for each unique record. Calculated server side.';
 
     ALTER TABLE activity_incoming_data ADD COLUMN activity_type VARCHAR(200) NULL;
-    COMMENT ON COLUMN activity_incoming_data.type IS 'Type of record';
-    CREATE index type_idx on activity_incoming_data (type);
+    COMMENT ON COLUMN activity_incoming_data.activity_type IS 'Type of record';
+    CREATE index type_idx on activity_incoming_data (activity_type);
 
     ALTER TABLE activity_incoming_data ADD COLUMN activity_sub_type VARCHAR(200) NULL;
-    COMMENT ON COLUMN activity_incoming_data.sub_type IS 'Sub Type of record';
-    CREATE index sub_type_idx on activity_incoming_data (sub_type);
+    COMMENT ON COLUMN activity_incoming_data.activity_sub_type IS 'Sub Type of record';
+    CREATE index sub_type_idx on activity_incoming_data (activity_sub_type);
 
     ALTER TABLE activity_incoming_data ADD COLUMN received_timestamp timestamp NOT NULL DEFAULT NOW();
     COMMENT ON COLUMN activity_incoming_data.received_timestamp IS 'The date and time data was received and inserted into the database.';
