77eb173748aef97e85392aa23273e79ddde63d50
Roland Stens <roland.stens@gmail.com>
2020-08-21 14:13:18 -0700

    Removing trailing whitespace

--- api/api_sources/sources/database/initial-data/InitialAdmins.ts
+++ api/api_sources/sources/database/initial-data/InitialAdmins.ts
@@ -82,7 +82,7 @@ export const InitialUsers = [
         additionalInitDataInfo: {
             roles: [1]
         }
-    }    
+    }
 ];
 
 // -------------------------------------------------------------
