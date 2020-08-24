6d2d52d4646199e780befcee2f9a90366d17a94b
Roland Stens <roland.stens@gmail.com>
2020-08-24 16:20:34 -0700

    Fix PVC Settings, volume size

--- api/.pipeline/lib/deploy.db.js
+++ api/.pipeline/lib/deploy.db.js
@@ -43,7 +43,8 @@ module.exports = (settings) => {
       'IMAGE_STREAM_NAME': name,
       'IMAGE_STREAM_VERSION': phases.build.tag,
       'POSTGRESQL_DATABASE': 'InvasiveBC',
-      'IMAGE_STREAM_NAMESPACE': phases.build.namespace
+      'IMAGE_STREAM_NAMESPACE': phases.build.namespace,
+      'VOLUME_CAPACITY': isStaticDeployment() ? "20Gi" : "3Gi"
     }
   }))
   
