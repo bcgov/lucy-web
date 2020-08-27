200b4ff14e7249986ecfafebfc49eedf2a27f3c9
Nick Phura <nickphura@gmail.com>
2020-08-27 10:57:19 -0700

    Update migrations

--- api-mobile/src/database/migrations/20200819074819_activity_view.ts
+++ api-mobile/src/database/migrations/20200819074819_activity_view.ts
@@ -1,41 +1,35 @@
-import * as Knex from "knex";
+import Knex from 'knex';
 
+const DB_SCHEMA = process.env.DB_SCHEMA || 'invasivesbc';
 
 export async function up(knex: Knex): Promise<void> {
-	
-	await knex.raw(`
-	    set schema 'invasivesbc';
-	    set search_path = invasivesbc,public;
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
 
-	CREATE OR REPLACE VIEW activity_common_fields_view as (
-	select 
-	activity_id,
-	activity_type,
-	activity_sub_type,
-	cast(activity_payload -> 'locationAndGeometry' ->> 'anchorPointX' as decimal) as anchor_point_x,
-	cast(activity_payload -> 'locationAndGeometry' ->> 'anchorPointY' as decimal) as anchor_point_y,
-	received_timestamp
+    -- ### Creating View: activity_common_fields_view ### --
 
-	from activity_incoming_data
-	);
+    CREATE OR REPLACE VIEW ${DB_SCHEMA}.activity_common_fields_view as (
+      select
+      activity_id,
+      activity_type,
+      activity_sub_type,
+      cast(activity_payload -> 'locationAndGeometry' ->> 'anchorPointX' as decimal) as anchor_point_x,
+      cast(activity_payload -> 'locationAndGeometry' ->> 'anchorPointY' as decimal) as anchor_point_y,
+      received_timestamp
 
-	COMMENT ON VIEW activity_common_fields_view IS 'View on fields common to all types of activities, with table activity_incoming_data as source.';
+      from ${DB_SCHEMA}.activity_incoming_data
+    );
 
-
-
-
-
-
-    `)
-    
+    COMMENT ON VIEW ${DB_SCHEMA}.activity_common_fields_view IS 'View on fields common to all types of activities, with table activity_incoming_data as source.';
+  `);
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.raw(`
-		    set schema 'invasivesbc';
-		    set search_path = invasivesbc,public;
-		    drop view activity_common_fields_view;
-	    `)
-}
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
 
+    DROP VIEW IF EXISTS activity_common_fields_view;
+  `);
+}
