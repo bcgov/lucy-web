b56a1c4651cbde864757ee0ece870616b4edee0d
Jamie Popkin <popkinj@littleearth.ca>
2020-09-15 16:12:10 -0700

    Better sql

--- api-mobile/src/queries/activity-queries.ts
+++ api-mobile/src/queries/activity-queries.ts
@@ -5,14 +5,41 @@ import { ActivityPostRequestBody, ActivitySearchCriteria } from './../models/act
  * SQL query to insert a new activity, and return the inserted record.
  *
  * @param {ActivityPostRequestBody} activityData
- * @returns {SQLStatement} sql query object
+ * @returns {string} sql query string
  */
-export const postActivitySQL = (activityData: ActivityPostRequestBody): SQLStatement => {
+
+// export const postActivitySQL = (activityData: ActivityPostRequestBody): SQLStatement => {
+export const postActivitySQL = (activityData: ActivityPostRequestBody) => {
   if (!activityData) {
     return null;
   }
 
-  return SQL`
+  console.log(`
+    INSERT INTO activity_incoming_data (
+      activity_type,
+      activity_sub_type,
+      received_timestamp,
+      activity_payload,
+      geog,
+      media_keys
+    ) VALUES (
+      '${activityData.activityType}',
+      '${activityData.activitySubType}',
+      '${activityData.date}',
+      '${JSON.stringify(activityData.activityPostBody,null,10)}',
+      public.ST_Force2D(
+        public.ST_SetSRID(
+          public.ST_GeomFromGeoJSON('${JSON.stringify(activityData.locationAndGeometry["geometry"])}')
+          ,4326
+        )
+      )::geography,
+      ${activityData.mediaKeys}
+    )
+    RETURNING
+      activity_incoming_data_id;
+  `);
+
+  return `
     INSERT INTO activity_incoming_data (
       activity_type,
       activity_sub_type,
