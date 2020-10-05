c0bf9ed5e9e9a6bb1c44bb97ec64b311aa6f6d6c
Nick Phura <nickphura@gmail.com>
2020-10-05 16:19:17 -0700

    30: Add more dropdown support to api-doc.json - sort api dropdowns

--- api-mobile/src/utils/api-doc-security-filter.ts
+++ api-mobile/src/utils/api-doc-security-filter.ts
@@ -15,38 +15,49 @@ import { cached } from './utils';
 export async function applyApiDocSecurityFilters(req: any) {
   const connection = await getDBConnection();
 
-  // fetch all code sets
-  const allCodeSets: IAllCodeSets = await cached(CacheKeys.AllCodeSets, 3600000, () => getAllCodeSets(connection))();
+  if (!connection) {
+    throw {
+      status: 503,
+      message: 'Failed to establish database connection'
+    };
+  }
 
-  // the apiDoc that updates will be applied to
-  const apiDoc = req['apiDoc'];
+  try {
+    // fetch all code sets
+    const allCodeSets: IAllCodeSets = await cached(CacheKeys.AllCodeSets, 3600000, () => getAllCodeSets(connection))();
 
-  let initialPass = true;
+    // the apiDoc that updates will be applied to
+    const apiDoc = req['apiDoc'];
 
-  // traverses the apiDoc object, calling the `cb` function for each level of the schema, the first being the entire
-  // (root) schema.
-  traverse(apiDoc, {
-    allKeys: true,
-    cb: (schema, jsonPtr) => {
-      if (initialPass) {
-        // first pass is always the entire (root) schema, which we can skip (for a minor efficiency improvement)
-        initialPass = false;
-        return;
+    let initialPass = true;
+
+    // traverses the apiDoc object, calling the `cb` function for each level of the schema, the first being the entire
+    // (root) schema.
+    traverse(apiDoc, {
+      allKeys: true,
+      cb: (schema, jsonPtr) => {
+        if (initialPass) {
+          // first pass is always the entire (root) schema, which we can skip (for a minor efficiency improvement)
+          initialPass = false;
+          return;
+        }
+
+        // apply code enum filters, if a matching `x-...` field exists
+        if (Object.keys(schema).includes(XApiDocKeys.XEnumCode)) {
+          // apply code enum filtering to this piece of schema
+          const updatedSchema = applyCodeEnumFilter(schema, allCodeSets);
+
+          // update apiDoc, replacing the old schema part with the updated schema part
+          jsonpatch.applyPatch(apiDoc, [{ op: 'replace', path: jsonPtr, value: updatedSchema }]);
+        }
       }
+    });
 
-      // apply code enum filters, if a matching `x-...` field exists
-      if (Object.keys(schema).includes(XApiDocKeys.XEnumCode)) {
-        // apply code enum filtering to this piece of schema
-        const updatedSchema = applyCodeEnumFilter(schema, allCodeSets);
-
-        // update apiDoc, replacing the old schema part with the updated schema part
-        jsonpatch.applyPatch(apiDoc, [{ op: 'replace', path: jsonPtr, value: updatedSchema }]);
-      }
-    }
-  });
-
-  // re-assign the updated apiDoc to the req
-  req['apiDoc'] = apiDoc;
+    // re-assign the updated apiDoc to the req
+    req['apiDoc'] = apiDoc;
+  } finally {
+    connection.release();
+  }
 
   return req;
 }
@@ -80,17 +91,39 @@ export function applyCodeEnumFilter(obj: object, allCodeSets: IAllCodeSets): obj
   // update the object, adding an `anyOf` field whose value is an array of enum objects
   obj = {
     ...obj,
-    anyOf: codes.map(code => {
-      return {
-        // the `type` specified by the field that contains the enum (the enum `type` must match the field `type`)
-        type: obj['type'],
-        // the column that contains the unique value for this code
-        enum: [code[xEnumCodeObj[XEnumCode.XEnumCodeValue]]],
-        // the column that contains the human readable name of this code
-        title: code[xEnumCodeObj[XEnumCode.XEnumCodeName]]
-      };
-    })
+    anyOf: codes
+      .map(code => {
+        return {
+          // the `type` specified by the field that contains the enum (the enum `type` must match the field `type`)
+          type: obj['type'],
+          // the column that contains the unique value for this code
+          enum: [code[xEnumCodeObj[XEnumCode.XEnumCodeValue]]],
+          // the column that contains the human readable name of this code
+          title: code[xEnumCodeObj[XEnumCode.XEnumCodeName]]
+        };
+      })
+      .sort(getAscObjectSorter('title'))
   };
 
   return obj;
+}
+
+/**
+ * Returns a comparator function that sorts objects in ascending order based on a specified `field`.
+ *
+ * @param {string} field the object field to sort based on
+ * @return {number}
+ */
+function getAscObjectSorter(field: string) {
+  return (a: object, b: object) => {
+    if (a[field] < b[field]) {
+      return -1;
+    }
+
+    if (a[field] > b[field]) {
+      return 1;
+    }
+
+    return 0;
+  };
 }
