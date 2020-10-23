d6d95082c4191574a6fa582f1c1c5b89f857ae6a
Jamie Popkin <popkinj@littleearth.ca>
2020-10-23 13:42:10 -0700

    Cleaning up and Swagger

--- api-mobile/src/paths/context/elevation.ts
+++ api-mobile/src/paths/context/elevation.ts
@@ -1,14 +1,9 @@
 'use strict';
 
 import axios from 'axios';
-import { ManagedUpload } from 'aws-sdk/clients/s3';
 import { RequestHandler } from 'express';
 import { Operation } from 'express-openapi';
-import { SQLStatement } from 'sql-template-strings';
-import { ALL_ROLES, WRITE_ROLES } from '../../constants/misc';
-import { getDBConnection } from '../../database/db';
-import { ActivityPostRequestBody, ActivitySearchCriteria, IMediaItem, MediaBase64 } from '../../models/activity';
-import { getActivitiesSQL, postActivitySQL } from '../../queries/activity-queries';
+import { ALL_ROLES } from '../../constants/misc';
 // import { uploadFileToS3 } from '../../utils/file-utils';
 import { getLogger } from '../../utils/logger';
 // import * as geoJSON_Feature_Schema from '../../openapi/geojson-feature-doc.json';
@@ -25,64 +20,28 @@ GET.apiDoc = {
       Bearer: ALL_ROLES
     }
   ],
-  requestBody: {
-    description: 'Activities search filter criteria object.',
-    content: {
-      'application/json': {
-        schema: {
-          properties: {
-            page: {
-              type: 'number',
-              default: 0,
-              minimum: 0
-            },
-            limit: {
-              type: 'number',
-              default: 25,
-              minimum: 0,
-              maximum: 100
-            },
-            activity_type: {
-              type: 'string'
-            },
-            activity_subtype: {
-              type: 'string'
-            },
-            date_range_start: {
-              type: 'string',
-              description: 'Date range start, in YYYY-MM-DD format. Defaults time to start of day.',
-              example: '2020-07-30'
-            },
-            date_range_end: {
-              type: 'string',
-              description: 'Date range end, in YYYY-MM-DD format. Defaults time to end of day.',
-              example: '2020-08-30'
-            },
-            // GeoJson Bounding Box
-            bbox: {
-              type: 'array',
-              minItems: 4,
-              items: {
-                type: 'number'
-              }
-            }
-          }
-        }
-      }
+  parameters: [
+    {
+      in: 'path',
+      name: 'lon',
+      required: true
+    },
+    {
+      in: 'path',
+      name: 'lat',
+      required: true
     }
-  },
+  ],
   responses: {
     200: {
-      description: 'Activity get response object array.',
+      description: 'Elevation value',
       content: {
         'application/json': {
           schema: {
-            type: 'array',
-            items: {
-              type: 'object',
-              properties: {
-                // Don't specify exact response, as it will vary, and is not currently enforced anyways
-                // Eventually this could be updated to be a oneOf list, similar to the Post request below.
+            type: 'object',
+            properties: {
+              elevation: {
+                type: 'integer'
               }
             }
           }
@@ -127,7 +86,7 @@ function getElevation(): RequestHandler {
 
     axios.get(url)
       .then((response) => {
-        return res.status(200).json(response.data);
+        return res.status(200).json({elevation: response.data?.altitude});
       })
       .catch((error) => {
         return defaultLog.debug({ label: 'getElevation', message: 'error', error });
