55a9d36fcf7869f7150eb08458e02d992ea69fb2
Roland Stens <roland.stens@gmail.com>
2020-12-22 14:17:46 -0800

    Prepare for OCP4

--- api-mobile/test/observation.sh
+++
@@ -1,4 +0,0 @@
-# TODO: Assign DB environment variables
-curl -X POST -d @./observation-polygon.json "localhost:3002/api/activity" \
---header "Content-Type: application/json" \
---header 'Authorization: Bearer '${1} \
