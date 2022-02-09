ed2635f2b137fc29d906021e98c43b7cc2154cea
Richard Tan <richard.tan@gov.bc.ca>
2022-02-09 11:16:04 -0800

    Add St Lawrence QC to list of waterbodies

---
+++ api/api_sources/schema-migration-sql/WaterBodySchema/WaterBodySchema-february2022.sql
@@ -0,0 +1,6 @@
+-- ## Inserting into table: water_body ## --
+-- ## Inserting Item: 2158  ## --
+INSERT INTO water_body(water_body_name,water_body_latitude,water_body_longitude,country_code,province_code,closest_city,distance)
+VALUES
+('St. Lawrence River','-69.716553','47.68725','CAN','QC','Quebec City','150');
+-- ## End of item: 2158 ## --
