f193419c94cb7e05f0581b1487c29de144824a3e
Richard Tan <richard.tan@gov.bc.ca>
2022-03-03 09:07:14 -0800

    Made the previous inspection days count nullable

---
+++ api/api_sources/schema-migration-sql/WatercraftRiskAssessmentSchema/WatercraftRiskAssessmentSchema-previousInspectionDays-20220303.up.sql
@@ -0,0 +1,8 @@
+-- ## Changing table: watercraft_risk_assessment
+-- ## Version: previousInspectionDays
+-- ## Info: Updating column previous_inspection_days_count
+-- ## Adding New Columns ## --
+
+-- ## Updating Column previous_inspection_days_count on table watercraft_risk_assessment
+ALTER TABLE watercraft_risk_assessment ALTER COLUMN previous_inspection_days_count DROP NOT NULL;
+-- ## --
