f77f8fc6ab5e0efd107dd0e84746c8ad6c3e4543
Dave <62899351+davidclaveau@users.noreply.github.com>
2024-01-25 14:50:57 -0800

    update to pascal case for linting (#1173)

--- api/api_sources/sources/database/migrations/1706036892274-closestCityNameChangeInWaterBody.ts
+++ api/api_sources/sources/database/migrations/1706036892274-closestCityNameChangeInWaterBody.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class closestCityNameChangeInWaterBody1706036892274 implements MigrationInterface {
+export class ClosestCityNameChangeInWaterBody1706036892274 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`UPDATE "water_body" SET "closest_city" = CONCAT('e.g. ', "closest_city")`);
     }
